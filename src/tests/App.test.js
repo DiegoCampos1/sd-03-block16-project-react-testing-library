122b5972dc3cc2b746af3f7fe4b25f2623781b32
Marco Barbosa <marco.meireles.b@gmail.com>
2020-05-21 17:01:33 -0300

    Testes para App.js feitos

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,28 +1,54 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render, cleanup } from '@testing-library/react';
-// import { render, cleanup, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { cleanup, fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+jest.mock('react-router-dom', () => {
+  const originalModule = jest.requireActual('react-router-dom');
+
+  return ({
+    ...originalModule,
+    BrowserRouter: ({ children }) => (<div>{children}</div>),
+  })
+})
 
 afterEach(cleanup);
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  const { getByText } = renderWithRouter(<App />);
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
 
 test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
-
+  const { getByText } = renderWithRouter(<App />);
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
 
+test('shows the Home link when the route is `/` and your destination page', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const home = getByText(/Home/i);
+  expect(home).toBeInTheDocument();
+  fireEvent.click(home);
+  const pathname = history.location.pathname;
+  expect(pathname).toBe('/');
+});
+
+test('shows the `About` link when the route is `/` and your destination page', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const about = getByText(/About/i);
+  expect(about).toBeInTheDocument();
+  fireEvent.click(about);
+  const pathname = history.location.pathname;
+  expect(pathname).toBe('/about');
+});
+
+test('shows the `Favorite Pokémons` link when the route is `/` and your destination page', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const favoritePokemons = getByText(/Favorite Pokémons/i);
+  expect(favoritePokemons).toBeInTheDocument();
+  fireEvent.click(favoritePokemons);
+  const pathname = history.location.pathname;
+  expect(pathname).toBe('/favorites');
+});
