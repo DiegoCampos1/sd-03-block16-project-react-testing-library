14e6b2a4f79d13cfaa900d05ae147b77215041a6
Hfreitas <hfreitas86@gmail.com>
2020-05-21 15:54:43 -0300

    Mockando history do react-router-dom e iniciando testes do requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,28 +1,38 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import { render, cleanup } from '@testing-library/react';
 import App from '../App';
 
 afterEach(cleanup);
 
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return {
+    ...render(<Router history={history}>{component}</Router>),
+    history,
+  };
+};
+
 describe('App.js tests', () => {
   test('renders a reading with the text `Pokédex`', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
+    const { getByText } = renderWithRouter(<App/>);
     const heading = getByText(/Pokédex/i);
     expect(heading).toBeInTheDocument();
   });
 
   test('shows the Pokédex when the route is `/`', () => {
-    const { getByText } = render(
-      <MemoryRouter initialEntries={['/']}>
-        <App />
-      </MemoryRouter>,
-    );
+    const { getByText } = renderWithRouter(<App />);
 
     expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  test('renders links navbar', () => {
+    const { getByText, getByRole, getAllByRole } = renderWithRouter(<App />);
+
+    const navBar = getByRole('navigation');
+    const {links} = navBar;
+    expect(navBar).toBeInTheDocument();
+    expect(navBar.links.length).toBe(3);
   });
 });
