0fc0e117910e56c9dcc5d03194efccb62d1dd47d
Henrique Rezende <henriquerezendec@gmail.com>
2020-05-21 19:57:04 -0300

    Teste do arquivo About.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('test file About.js', () => {
+  test('Contain a h2 heading with "About Pokédex" text', () => {
+    const { getByText } = render(<About />);
+    const heading = getByText('About Pokédex');
+    expect(heading).toBeInTheDocument();
+    expect(heading.tagName).toBe('H2');
+  });
+
+  test('Contain 2 paragraphs about Pokédex', () => {
+    render(<About />);
+    const paragraphs = document.querySelectorAll('p');
+    expect(paragraphs.length).toBe(2);
+  });
+
+  test('Contain Pokédex image', () => {
+    const { getByAltText } = render(<About />);
+    const img = getByAltText('Pokédex');
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+afterEach(cleanup);
+
+describe('test file App.js', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('check if button `home` exist and redirect to path `/`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const homeLinkText = getByText(/home/i);
+    expect(homeLinkText).toBeInTheDocument();
+    fireEvent.click(homeLinkText);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/');
+  });
+
+  test('check if button `about` exist and redirect to path `/about`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const aboutLinkText = getByText(/about/i);
+    expect(aboutLinkText).toBeInTheDocument();
+    fireEvent.click(aboutLinkText);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/about');
+  });
+
+  test('check if button `Favorite Pokémons` exist and redirect to path `/favorites`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const favoriteLinkText = getByText(/favorite pokémons/i);
+    expect(favoriteLinkText).toBeInTheDocument();
+    fireEvent.click(favoriteLinkText);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/favorites');
+  });
 });
