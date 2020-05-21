b28a55d2dc67952dee1c32f92f3fd3f1110a56b5
Gabriel <dreadpool38@gmail.com>
2020-05-21 17:46:19 -0300

    favorite

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+
+test('favorite is empty', () => {
+  const { getByText } = render(<FavoritePokemons />);
+  const phrase = getByText('No favorite pokemon found');
+  expect(phrase).toBeInTheDocument();
+});
+
+test('favorite pokemons', () => {
+  render(<FavoritePokemons />);
+  const pokemons = document.querySelectorAll('.pokemon');
+  pokemons.forEach((pokemon) => {
+    const fav = pokemon.querySelector('.favorite-icon');
+    expect(fav).toBeInTheDocument();
+  });
+});
