9ae9251feb318f7c3eafc21de26d168c8b8a8426
nato-x <alvares.renato@hotmail.com>
2020-05-21 19:30:57 -0300

    Nato React Testing project, About, App and Favorites tested

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+test('', () => {
+  const { getByText, getByRole } = render(
+    <MemoryRouter initialEntries={[{ pathname: '/about' }]}>
+      <App />
+    </MemoryRouter>,
+  );
+  expect(getByText('About Pokédex')).toBeInTheDocument();
+  const p1 = getByText(/This application simulates a Pokédex,/);
+  const p2 = getByText(/One can filter Pokémons by type/);
+  expect(p1).toBeInTheDocument();
+  expect(p2).toBeInTheDocument();
+  const image = getByRole('img');
+  expect(image.src).toMatch('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
