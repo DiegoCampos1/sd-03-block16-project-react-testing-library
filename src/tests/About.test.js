c504a2e9b55e8f6a77079fea45a8170e40d02e4f
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-05-21 17:30:39 -0300

    About.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render, cleanup } from '@testing-library/react';
+import About from '../components/About';
+
+
+function renderWithRouter(
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+afterEach(cleanup);
+
+test('testing information about Pokedex', () => {
+  const { getByText, getByRole, queryAllByRole } = renderWithRouter(<About />);
+
+  const heading = getByRole('heading');
+  expect(heading).toBeInTheDocument();
+  expect(heading.tagName).toBe('H2');
+
+  const pokedexText = getByText('About Pokédex');
+  expect(pokedexText).toBeInTheDocument();
+
+  const paragraphs = queryAllByRole('region');
+  expect(paragraphs.length).toBe(2);
+
+  const img = getByRole('img');
+  expect(img.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
