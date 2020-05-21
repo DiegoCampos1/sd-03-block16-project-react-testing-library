4bf0bcdd9b9d77fd9035a00afdc065d8c140ddeb
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-05-21 17:04:28 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,44 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('tests About.js', () => {
+  test('shows the About Pokédex', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/about']}>
+        <About />
+      </MemoryRouter>
+    );
+
+    const heading = getByText('About Pokédex');
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('About Pokédex has 2 `p` tags', () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={['/about']}>
+        <About />
+      </MemoryRouter>
+    );
+
+    const paragraphs = container.querySelectorAll('p');
+
+    expect(paragraphs.length).toBe(2);
+  });
+
+  test('About Pokédex has 2 `p` tags', () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={['/about']}>
+        <About />
+      </MemoryRouter>
+    );
+
+    const img = container.querySelector('img');
+
+    expect(img.src).toBe(
+      'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+    );
+  });
+});
