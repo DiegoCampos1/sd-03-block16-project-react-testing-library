218a3e90c6dc1a867ff6bd52b50d54b3c3fc4f6c
tales <talestales@gmail.com>
2020-05-21 16:23:44 -0300

    b

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+import { About } from '../components';
+
+test('renders a reading with the text `About Pokédex`', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+      <About />
+    </MemoryRouter>,
+  );
+  expect(getByText('About Pokédex')).toBeInTheDocument();
+});
+
+test('look for the image', () => {
+  const { findAllBy } = render(
+    <MemoryRouter>
+      <App />
+      <About />
+    </MemoryRouter>,
+  );
+  expect(findAllBy('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex')).toBeInTheDocument();
+  });
