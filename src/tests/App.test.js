8cd82f6d63860d10bc3ca270f72d75322b0e7637
Henrique Rezende <henriquerezendec@gmail.com>
2020-05-21 18:49:52 -0300

    Teste do arquivo App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
+
+afterEach(cleanup);
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -11,4 +14,31 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test('check if button `home` exist and redirect to path `/`', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const homeLinkText = getByText(/home/i);
+  expect(homeLinkText).toBeInTheDocument();
+  fireEvent.click(homeLinkText);
+  const { location: { pathname } } = history;
+  expect(pathname).toBe('/');
+});
+
+test('check if button `about` exist and redirect to path `/about`', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const aboutLinkText = getByText(/about/i);
+  expect(aboutLinkText).toBeInTheDocument();
+  fireEvent.click(aboutLinkText);
+  const { location: { pathname } } = history;
+  expect(pathname).toBe('/about');
+});
+
+test('check if button `Favorite Pokémons` exist and redirect to path `/favorites`', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const favoriteLinkText = getByText(/favorite pokémons/i);
+  expect(favoriteLinkText).toBeInTheDocument();
+  fireEvent.click(favoriteLinkText);
+  const { location: { pathname } } = history;
+  expect(pathname).toBe('/favorites');
 });
