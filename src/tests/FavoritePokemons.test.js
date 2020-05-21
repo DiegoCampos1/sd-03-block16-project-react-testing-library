66b132d8262f1bfe0af4970d98a0e1cd8ecbf5ac
tales <talestales@gmail.com>
2020-05-21 17:29:54 -0300

    c

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
-import { FavoritePokémons } from '../components';
+import { FavoritePokemons } from '../components';
 
 test('renders a reading with the text `No found`', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
-      <FavoritePokémons />
+      <FavoritePokemons />
     </MemoryRouter>,
   );
   expect(getByText('No favorite pokemon found')).toBeInTheDocument();
