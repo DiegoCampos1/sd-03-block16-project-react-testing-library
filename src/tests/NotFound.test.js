cb235b27adac998c861e4105efc9c86f15fc2017
tales <talestales@gmail.com>
2020-05-21 17:39:02 -0300

    d

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+import { NotFound } from '../components';
+
+test('renders a reading with the text `NotFound`', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+      <NotFound />
+    </MemoryRouter>,
+  );
+  expect(getByText('Page requested not found')).toBeInTheDocument();
+});
+
+test('look for the image', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+      <NotFound />
+    </MemoryRouter>,
+  );
+  expect(getByText('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+  ))
+    .toBeInTheDocument();
+});
