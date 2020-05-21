3ecdc6fb566a75adba0d2992204c94744e46b69f
Gustaff <gustavohmfigueiredo@gmail.com>
2020-05-21 16:58:25 -0300

    confirmando h2 atraves de chaves do react

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { cleanup, render } from '@testing-library/react';
+import About from '../components/About';
+
+const renderWithMemoryRouter = (component, initialHistoy) => {
+  return (render(
+    <MemoryRouter initialEntries={initialHistoy}>
+      {component}
+    </MemoryRouter>
+  ));
+}
+
+describe('About', () => {
+  afterEach(cleanup);
+
+  describe('information about pokedex', () => {
+    test('should exist', () => {
+      const { getByText } = render(<About />);
+      const aboutText1 = 'This application simulates a Pokédex, ' +
+        'a digital encliclopedia containing all Pokémons';
+      const aboutText2 = 'One can filter Pokémons by type, ' +
+        'and see more details for each one of them';
+      expect(getByText(aboutText1)).toBeInTheDocument();
+      expect(getByText(aboutText2)).toBeInTheDocument();
+    });
+
+    test('should have an h2 heading with the About Pokédex text', () => {
+      const { getByText } = render(<About />);
+      const h2 = getByText('About Pokédex');
+      const h2Keys = Object.keys(h2);
+      expect(h2).toBeInTheDocument();
+      expect(h2[h2Keys[0]].type).toBe('h2');
+    });
+  });
+});
