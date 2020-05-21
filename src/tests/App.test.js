e2b7c51cea37b0cedc55132db3ce7163087178c1
Gustaff <gustavohmfigueiredo@gmail.com>
2020-05-21 16:16:32 -0300

    test of the links ok

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -36,8 +36,8 @@ describe('should show the main page of Pokedex', () => {
   });
 });
 
-describe('links in the top', () => {
-  test('in the Home page', () => {
+describe('exist on the top links to', () => {
+  test('Home page', () => {
     const { getByText } = render(
       <MemoryRouter initialEntries={['/']}>
         <App/>
@@ -48,7 +48,7 @@ describe('links in the top', () => {
     expect(getByText('Favorite Pokémons')).toBeInTheDocument();
   });
 
-  test('in the details page', () => {
+  test('details page', () => {
     const { getByText } = render(
       <MemoryRouter initialEntries={['/pokemons/25'] /* Pikachu details*/}>
         <App/>
@@ -59,7 +59,7 @@ describe('links in the top', () => {
     expect(getByText('Favorite Pokémons')).toBeInTheDocument();
   });
 
-  test('in the favorites page', () => {
+  test('favorites page', () => {
     const { getByText } = render(
       <MemoryRouter initialEntries={['/favorites']}>
         <App/>
@@ -70,7 +70,7 @@ describe('links in the top', () => {
     expect(getByText('Favorite Pokémons')).toBeInTheDocument();
   });
 
-  test('in the about page', () => {
+  test('about page', () => {
     const { getByText } = render(
       <MemoryRouter initialEntries={['/about']}>
         <App/>
@@ -79,5 +79,52 @@ describe('links in the top', () => {
     expect(getByText('Home')).toBeInTheDocument();
     expect(getByText('About')).toBeInTheDocument();
     expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+  });
+});
+
+describe('clicking on link', () => {
+  const HomeText = 'Encountered pokémons';
+  const AboutText = 'This application simulates a Pokédex, ' +
+    'a digital encliclopedia containing all Pokémons';
+  const FavoriteText = 'Favorite pokémons';
+  test('Home', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <App/>
+      </MemoryRouter>
+    );
+    expect(getByText(HomeText)).toBeInTheDocument();
+
+    fireEvent.click(getByText('Home'));
+
+    expect(getByText(HomeText)).toBeInTheDocument();
+  });
+
+  test('About', () => {
+    const { getByText, queryByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <App/>
+      </MemoryRouter>
+    );
+    expect(getByText(HomeText)).toBeInTheDocument();
+
+    fireEvent.click(getByText('About'));
+
+    expect(queryByText(HomeText)).not.toBeInTheDocument();
+    expect(getByText(AboutText)).toBeInTheDocument();
+  });
+
+  test('Favorite Pokémons', () => {
+    const { getByText, queryByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <App/>
+      </MemoryRouter>
+    );
+    expect(getByText(HomeText)).toBeInTheDocument();
+
+    fireEvent.click(getByText('Favorite Pokémons'));
+
+    expect(queryByText(HomeText)).not.toBeInTheDocument();
+    expect(getByText(FavoriteText)).toBeInTheDocument();
   });
 });
