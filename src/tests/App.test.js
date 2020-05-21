0368fa62b505434279b53df318f00feedc1fe1dc
fabemiliano <fab.emiliano@gmail.com>
2020-05-21 17:57:01 -0300

    testing first requiste

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, cleanup } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+afterEach(cleanup);
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  const { getByText } = renderWithRouter(<App />);
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test('tests if home path is right', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const home = getByText('Home');
+  fireEvent.click(home);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+});
+
+test('tests if about path is right', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const about = getByText('About');
+  fireEvent.click(about);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+});
+
+test('tests if favorite path is right', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const favorites = getByText('Favorite Pokémons');
+  fireEvent.click(favorites);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
+});
+
+test('tests non existing page', () => {
+  const { getByText } = renderWithRouter(<App />, { route: '/xablau' });
+  const pageNotFound = getByText('Page requested not found');
+  expect(pageNotFound).toBeInTheDocument();
 });
