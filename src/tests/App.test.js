3c4ae6ba907fa521b6e2dd00e9f2bee76adfbda3
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-05-21 16:36:10 -0300

    positivos?

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,23 +1,7 @@
 import React from 'react';
-import { MemoryRouter, Router } from 'react-router-dom';
-import { render, cleanup } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
 import App from '../App';
-
-afterEach(cleanup);
-
-function renderWithRouter(
-  ui,
-  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
-) {
-  return {
-    ...render(<Router history={history}>{ui}</Router>),
-    // adding `history` to the returned utilities to allow us
-    // to reference it in our tests (just try to avoid using
-    // this to test implementation details).
-    history,
-  };
-}
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -27,16 +11,4 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
-});
-
-describe('No topo da aplicação, deve haver um conjunto fixo de links de navegação', () => {
-  it('O primeiro link deve possuir o texto Home com a URL /', () => {
-    const { getByText } = renderWithRouter(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
-    const home = getByText(/Home/i);
-    expect(home).toBeInTheDocument();
-  });
 });
