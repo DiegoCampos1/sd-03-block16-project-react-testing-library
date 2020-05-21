2d1bff913d666a40bc9536a84d9ece2388011ff7
tales <talestales@gmail.com>
2020-05-21 17:17:09 -0300

    c

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+import { Favorite } from '../components';
+
+test('renders a reading with the text `No found`', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+      <Favorite />
+    </MemoryRouter>,
+  );
+  expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+});
