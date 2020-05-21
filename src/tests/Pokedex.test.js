2943ddebc42f251716e2691d7699ab390366623a
Jafet Henrique <jafetguerra6@gmail.com>
2020-05-21 19:00:34 -0300

    requisito 5 feito

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, fireEvent, getAllByTestId } from '@testing-library/react';
+import renderWithRouter from './testService';
+import App from '../App';
+import pokemons from '../data';
+
+afterEach(cleanup);
+
+test('testing next pokémon button', () => {
+  const { getByText, getByTestId } = renderWithRouter(<App />);
+
+  const nextPokeButton = getByText('Próximo pokémon');
+  expect(nextPokeButton).toBeInTheDocument();
+  pokemons.forEach((e) => {
+    const pokemonName = getByTestId('pokemon-name');
+    expect(pokemonName).toHaveTextContent(e.name);
+    fireEvent.click(nextPokeButton);
+  });
+});
