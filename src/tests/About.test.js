9fa1b4e67f19504b1e9f6bf7e840cad20f60d9b0
Jafet Henrique <jafetguerra6@gmail.com>
2020-05-21 17:16:00 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import About from '../components/About';
+
+
+function renderWithRouter(
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+afterEach(cleanup);
+
+test('testing information about Pokedex', () => {
+  const { getByText, getByRole, queryAllByRole } = renderWithRouter(<About />);
+
+  const h2Exists = getByRole('heading');
+  expect(h2Exists).toBeInTheDocument();
+  expect(h2Exists.tagName).toBe('H2');
+
+  const pokedexText = getByText('About Pokédex');
+  expect(pokedexText).toBeInTheDocument();
+
+  const allParagraphs = queryAllByRole('region');
+  expect(allParagraphs.length).toBe(2);
+
+  const imgPokedex = getByRole('img');
+  expect(imgPokedex.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png')
+});
