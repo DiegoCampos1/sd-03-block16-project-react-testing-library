f88936018c551db42a2302f622d369d8c7afeaf1
Anonymous <Anonymous>
2020-05-22 14:13:39 +0000

    testing repl and FavoritePokemons page

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import App from '../App';
+
+afterEach(cleanup);
+
+test('testing favorite pokémons page', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+ );
+
+  const favPokeLink = getByText(/Favorite/i);
+  fireEvent.click(favPokeLink);
+  const noFavFound = getByText('No favorite pokemon found');
+  expect(noFavFound).toBeInTheDocument();
+
+  const homeLink = getByText(/Home/i);
+  fireEvent.click(homeLink);
+
+  const detailsLink = getByText(/More details/i);
+  fireEvent.click(detailsLink);
+  const checkboxFav = getByText(/Pokémon favoritado?/i);
+  fireEvent.click(checkboxFav);
+  fireEvent.click(favPokeLink);
+  const pokemonFav = getByText(/Pikachu/i);
+  expect(pokemonFav).toBeInTheDocument();
+});
