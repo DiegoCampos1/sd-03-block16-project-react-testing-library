70de4f388388c27324cd0a8ac6acc55fe4dfa900
William Igor <williamigorsilva@gmail.com>
2020-05-22 15:57:58 -0300

    Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
+import { fireEvent, getByText } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
-import { Pokedex } from '../components';
-
+import App from '../App';
+import Pokedex from '../components/Pokedex';
+import pokemon from '../data';
 
 // test('Next Pokemon button', () => {
 //   const { getByText } = renderWithRouter(<Pokedex />);
@@ -11,15 +12,46 @@ import { Pokedex } from '../components';
 // });
 
 test('Value Button Pokemon', () => {
-  const { dataTestId } = renderWithRouter(<Pokedex />);
-  const btn = dataTestId('next-pokemon');
+  const { getByText, getByTestId } = renderWithRouter(<App />);
+  const btn = getByText(/Próximo pokémon/i);
+  // const btn = dataTestId('next-pokemon');
   expect(btn).toBeInTheDocument();
-  expect(btn.value).toBe('Próximo pokémon');
+  fireEvent.click(btn);
+  // expect(btn.value).toBe('Próximo pokémon');
+  pokemon.forEach(elem => {
+    const pokemonName = getByTestId('pokemon-name');
+    expect(pokemonName).toBeInTheDocument(elem.name);
+    fireEvent.click(btn);
+  })
 });
 
-test('CLick successives in button', () => {
-  const { getByRole } = renderWithRouter(<Pokedex />);
-  const clickBtn = getByRole('button');
-  fireEvent.click(clickBtn);
-  expect(clickBtn).toBe('');
+test('Test buttons for each title', () => {
+  // const everyButtons = ['All', 'Eletric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon' ];
+  const { getAllByText } = renderWithRouter(<App/>);
+  pokemon.map(elem =>{
+    const buttonType = getAllByText('All', 'Eletric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon' );
+    expect(buttonType).toBeInTheDocument(elem);
+  })
+});
+
+// test('CLick successives in button', () => {
+//   const { getByRole } = renderWithRouter(<App />);
+//   const clickBtn = getByRole('button');
+//   fireEvent.click(clickBtn);
+//   const { getByText } = renderWithRouter(<App />);
+//   const clickSuccessives = getByText(/Average weight/i);
+//   expect(clickSuccessives).toBeInTheDocument();
+// });
+
+test('Last pokemon return to firts', () => {
+  const { queryAllByAltText } = renderWithRouter(<App />);
+  fireEvent.click(queryAllByAltText('button'));
+  const firstPokemon = getByText(/Eletric/i);
+  expect(firstPokemon).toBeInTheDocument();
+});
+
+test('Pokedex one pokemon in page', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const indexCard = getByText(/More details/i);
+  expect(indexCard.length).toBe(1);
 });
