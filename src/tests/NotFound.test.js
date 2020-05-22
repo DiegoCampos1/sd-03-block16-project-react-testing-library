b9f810613bf7e2f3456b3c4912a963034fb00a1b
Rodrigo Mlitão <rodrigo_militao@hotmail.com.br>
2020-05-22 20:03:38 +0000

    NotFound page test

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { cleanup } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+function renderWithRouter(
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+afterEach(cleanup);
+
+test('testing when a url does not match', () => {
+  const { getByText, getByRole, getByAltText } = renderWithRouter(<NotFound />);
+
+  const h2Exists = getByRole('heading');
+  expect(h2Exists).toBeInTheDocument();
+  expect(h2Exists.tagName).toBe('H2');
+
+  const notFoundPhrase = getByText('Page requested not found');
+  expect(notFoundPhrase).toBeInTheDocument();
+
+  const img = getByAltText('Pikachu crying because the page requested was not found');
+  expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
