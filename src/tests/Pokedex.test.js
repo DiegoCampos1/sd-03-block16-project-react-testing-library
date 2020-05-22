7f3d39ea6f43be830e7ab79833045e13f00d4394
henrique <hbaetaleite@gmail.com>
2020-05-22 20:33:53 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('', () => {
+  test('Botão de próximo Pokemon', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const btn = getByText('Próximo pokémon');
+    expect(btn).toBeInTheDocument();
+  
+    fireEvent.click(btn);
+  
+    const nextPokemon = getByText('Charmander');
+    expect(nextPokemon).toBeInTheDocument();
+  
+  });
+
+  test('Pokedex deve conter botão para resetar filtro', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const btnAll = getByText('All');
+    expect(btnAll).toBeInTheDocument();
+    fireEvent.click(btnAll);
+
+    expect(getByText('Pikachu')).toBeInTheDocument();
+  });
+
+});
+
+describe('outra suit', () => {
+  test('botao2', () => {
+
+  })
+});
