f447f7a865bf2fd5d64db6a94ae618a73eeb4f35
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-05-22 11:45:03 -0300

    cc

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render, cleanup, fireEvent } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
 
 test('testing favorite pokémons page', () => {
