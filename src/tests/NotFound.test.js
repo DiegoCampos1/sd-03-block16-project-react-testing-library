0a4440ceea5ea9c7720263903ad11b0dbf0b9e16
William Igor <williamigorsilva@gmail.com>
2020-05-22 00:26:56 -0300

    Page not found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../components';
+
+test('Page container h2', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+  const getTitleH2 = getByText(/Page requested not found/i);
+  expect(getTitleH2).toBeInTheDocument();
+});
+
+test('Img in page', () => {
+  const { getByAltText } = renderWithRouter(<NotFound />);
+  const img = getByAltText('Pikachu crying because the page requested was not found');
+  expect(img).toBeInTheDocument();
+  expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+})
