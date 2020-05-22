cff562e9d98500db2ab9fbef452c4f8ad6e422e0
Gustaff <gustavohmfigueiredo@gmail.com>
2020-05-22 15:23:19 -0300

    aquivos do strike e testes iniciais de PokemonDetails

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,62 @@
-test('', () => {});
+import React from 'react';
+import { render, cleanup, getByText } from '@testing-library/react';
+
+import PokemonDetails from '../components/PokemonDetails';
+
+import pokemons from '../data';
+
+describe('PokemonDetails', () => {
+  afterEach(cleanup);
+  const match = { params: { id: '25' }}
+
+  test('should have information only about the selected pokémon', () => {
+    const { getByText } = render(
+      <PokemonDetails
+        match={match}
+        isPokemonFavoriteById={{ 25: true }}
+        onUpdateFavoritePokemons={() => null}
+        pokemons={[pokemons[0]]}
+      />
+    );
+    expect(getByText('Pikachu Details')).toBeInTheDocument();
+  });
+
+  test('should NOT have a link for details', () => {
+    const { queryByText } = render(
+      <PokemonDetails
+        match={match}
+        isPokemonFavoriteById={{ 25: true }}
+        onUpdateFavoritePokemons={() => null}
+        pokemons={[pokemons[0]]}
+      />
+    );
+    expect(queryByText('More details')).not.toBeInTheDocument();
+  });
+
+  test('should have an Summary h2', () => {
+    const { queryByText } = render(
+      <PokemonDetails
+        match={match}
+        isPokemonFavoriteById={{ 25: true }}
+        onUpdateFavoritePokemons={() => null}
+        pokemons={[pokemons[0]]}
+      />
+    );
+    expect(queryByText('Summary')).toBeInTheDocument();
+    expect(queryByText('Summary')).toContainHTML('<h2>');
+  });
+
+  test('should have a paragrph with the PikachuDetails', () => {
+    const mockedPikachuDescription = 'This intelligent Pokémon roasts hard berries ' +
+      'with electricity to make them tender enough to eat.';
+    const { getByText } = render(
+      <PokemonDetails
+        match={match}
+        isPokemonFavoriteById={{ 25: true }}
+        onUpdateFavoritePokemons={() => null}
+        pokemons={[pokemons[0]]}
+      />
+    );
+    expect(getByText(mockedPikachuDescription)).toBeInTheDocument();
+  });
+});
