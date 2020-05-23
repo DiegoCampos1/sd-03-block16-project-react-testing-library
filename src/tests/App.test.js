1eb524ae36305914e46ca7a77b9ed4a48831c10f
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-05-23 12:13:51 -0300

    CC Issues

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import renderWithRouter from '../renderWithRouter';
 import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('tests App.js', () => {
