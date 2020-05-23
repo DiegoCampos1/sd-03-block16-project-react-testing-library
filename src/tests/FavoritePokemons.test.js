e29e2af28daa397654b0b6897ee38d3a7a6b8ed1
rsfdev <roger_faustino@hotmail.com>
2020-05-23 15:22:02 -0300

    Two more files

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,50 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import data from '../data';
+import isPokemonFavoriteById from '../components/mocks';
+
+const favoritePokemon = data.filter(({ id }) => isPokemonFavoriteById[id]);
+const notFavoritePokemon = data.filter(({ id }) => !isPokemonFavoriteById[id]);
+
+describe('FavoritePokemons.js', () => {
+  test('Favorite Pokemon', () => {
+    const { getByText } = renderWithRouter(
+      <FavoritePokemons pokemons={favoritePokemon} />,
+      { route: '/favorites' },
+    );
+    const heading = getByText('Favorite pokémons');
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('notFound text in FavoritePokemons', () => {
+    const { getByText } = renderWithRouter(
+      <FavoritePokemons pokemons={[]} />,
+      { route: '/favorites' },
+    );
+    const notFound = getByText('favorite pokemon not found');
+    expect(notFound).toBeInTheDocument();
+  });
+
+  test('favorite pokemon card', () => {
+    const { getByText, container } = renderWithRouter(
+      <FavoritePokemons pokemons={favoritePokemon} />,
+      { route: '/favorites' },
+    );
+    const cards = container.querySelectorAll('.favorite-pokemon');
+    expect(cards.length).toBe(5);
+    favoritePokemon.forEach(({ name }) => {
+      expect(getByText(name)).toBeInTheDocument();
+    });
+  });
+
+  test('favorite pokemon card > FavoritePokemons page', () => {
+    const { queryByText } = renderWithRouter(
+      <FavoritePokemons pokemons={favoritePokemon} />,
+      { route: '/favorites' },
+    );
+    notFavoritePokemon.forEach(({ name }) => {
+      expect(queryByText(name)).toBeNull();
+    });
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('NotFound.js testing', () => {
+  test('Finding h2 heading', () => {
+    const { getByText } = renderWithRouter(<NotFound />, { route: '/not-found' });
+    const notFound = getByText('Page requested not found');
+    expect(notFound).toBeInTheDocument();
+    expect(notFound.tagName).toBe('H2'); // Using tagname
+  });
+
+  test('NotFound image', () => {
+    const { container } = renderWithRouter(<NotFound />, { route: '/not-found' });
+    const img = container.querySelector('IMG');
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
