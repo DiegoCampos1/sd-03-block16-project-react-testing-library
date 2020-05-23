498c2c6eb7dc7d2c57b7f4e9953e48572dd5cfc8
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-05-23 15:31:50 -0300

    Requisito

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { fireEvent } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
-import Pokedex from '../components/Pokedex';
+import App from '../App';
 import data from '../data';
-import isPokemonFavoriteById from '../components/mocks';
 
 const pokemonTypes = [
   ...new Set(data.reduce((types, { type }) => [...types, type], [])),
@@ -12,7 +11,7 @@ const pokemonTypes = [
 describe('tests Pokedex.js', () => {
   test('shows next pokemon button in Pokedex page', () => {
     const { getByText } = renderWithRouter(
-      <Pokedex pokemons={data} isPokemonFavoriteById={isPokemonFavoriteById} />,
+      <App />,
     );
 
     const nextPokemon = getByText('Próximo pokémon');
@@ -22,7 +21,7 @@ describe('tests Pokedex.js', () => {
 
   test('next pokemon button show next pokemon', () => {
     const { getByText } = renderWithRouter(
-      <Pokedex pokemons={data} isPokemonFavoriteById={isPokemonFavoriteById} />,
+      <App />,
     );
 
     const nextPokemon = getByText('Próximo pokémon');
@@ -35,7 +34,7 @@ describe('tests Pokedex.js', () => {
 
   test('after last pokemon a new click should show the first pokemon', () => {
     const { getByText } = renderWithRouter(
-      <Pokedex pokemons={data} isPokemonFavoriteById={isPokemonFavoriteById} />,
+      <App />,
     );
 
     const nextPokemon = getByText('Próximo pokémon');
@@ -46,7 +45,7 @@ describe('tests Pokedex.js', () => {
 
   test('should show just one pokemon at once', () => {
     const { getByText, getAllByText } = renderWithRouter(
-      <Pokedex pokemons={data} isPokemonFavoriteById={isPokemonFavoriteById} />,
+      <App />,
     );
 
     const moreDetails = getAllByText('More details');
@@ -57,7 +56,7 @@ describe('tests Pokedex.js', () => {
 
   test('Pokedex page should have filter buttons with the name equal the type', () => {
     const { getAllByTestId } = renderWithRouter(
-      <Pokedex pokemons={data} isPokemonFavoriteById={isPokemonFavoriteById} />,
+      <App />,
     );
 
     pokemonTypes.forEach((type, index) => {
@@ -69,7 +68,7 @@ describe('tests Pokedex.js', () => {
 
   test('clink in a filter button should shows just pokemons of that type', () => {
     const { getByText, getAllByTestId } = renderWithRouter(
-      <Pokedex pokemons={data} isPokemonFavoriteById={isPokemonFavoriteById} />,
+      <App />,
     );
 
     const nextPokemon = getByText('Próximo pokémon');
@@ -90,7 +89,7 @@ describe('tests Pokedex.js', () => {
 
   test('Pokedex page should have a reset button', () => {
     const { getByText } = renderWithRouter(
-      <Pokedex pokemons={data} isPokemonFavoriteById={isPokemonFavoriteById} />,
+      <App />,
     );
 
     const all = getByText('All');
@@ -100,7 +99,7 @@ describe('tests Pokedex.js', () => {
 
   test('click in All button should show all the pokemons', () => {
     const { getByText } = renderWithRouter(
-      <Pokedex pokemons={data} isPokemonFavoriteById={isPokemonFavoriteById} />,
+      <App />,
     );
 
     const all = getByText('All');
@@ -115,7 +114,7 @@ describe('tests Pokedex.js', () => {
 
   test('verify if the all button is load first', () => {
     const { getByText } = renderWithRouter(
-      <Pokedex pokemons={data} isPokemonFavoriteById={isPokemonFavoriteById} />,
+      <App />,
     );
 
     const nextPokemon = getByText('Próximo pokémon');
@@ -129,7 +128,7 @@ describe('tests Pokedex.js', () => {
 
   test('Pokedex page should a button filter for each type of pokemon', () => {
     const { getByText, getAllByText } = renderWithRouter(
-      <Pokedex pokemons={data} isPokemonFavoriteById={isPokemonFavoriteById} />,
+      <App />,
     );
 
     pokemonTypes.forEach((type) => {
@@ -140,7 +139,7 @@ describe('tests Pokedex.js', () => {
 
   test('next pokemon button should disabled if theres only one pokemon', () => {
     const { getByText } = renderWithRouter(
-      <Pokedex pokemons={data} isPokemonFavoriteById={isPokemonFavoriteById} />,
+      <App />,
     );
 
     const nextPokemon = getByText('Próximo pokémon');
