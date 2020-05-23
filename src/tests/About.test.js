486c5e2680a1a7f4d9ab2dcb3e28f9720b4f36ea
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-05-22 21:22:44 -0300

    Requisito 2 finalizado

---
+++ src/tests/About.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Testes do arquivo About.js', () => {
+  test('Componente deve conter os textos...', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const heading = getByText('About Pokédex');
+    expect(heading).toBeInTheDocument();
+    const paragraph1 = getByText(
+      'This application simulates a Pokédex, a digital encliclopedia containing all Pokémons'
+    );
+    const paragraph2 = getByText(
+      'One can filter Pokémons by type, and see more details for each one of them'
+    );
+  });
+
+  test('Componente deve conter a imagem especifica', () => {
+    const { getByAltText } = renderWithRouter(<About />);
+    const altImg = getByAltText('Pokédex');
+    expect(altImg).toBeInTheDocument();
+    const image = altImg.src;
+    expect(image).toBe(
+      'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+    );
+  });
+});
