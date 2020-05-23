a107666f9dadc94cd1229162c1f153d213dd547c
lcdlima <leticia.duarte.lima@gmail.com>
2020-05-23 06:48:47 -0300

    Commiting to change branch

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,49 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { cleanup, render, fireEvent } from '@testing-library/react';
 import App from '../App';
+
+afterEach(cleanup);
+
+test('shows the Pokédex when the route is `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('Fixed navigation links', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const homeElement = getByText('Home');
+  expect(homeElement.getAttribute("href")).toBe('/');
+
+  const aboutElement = getByText('About');
+  expect(aboutElement.getAttribute("href")).toBe('/about');
+
+  const favElement = getByText('Favorite Pokémons');
+  expect(favElement.getAttribute("href")).toBe('/favorites');
+})
+
+test('Click in Home', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const homeElement = getByText('Home');
+  fireEvent.click(homeElement);
+  const pathname = history.location.pathname;
+  expect(pathname).toBe(`/`);
+})
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -13,12 +55,3 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
-
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
-});
