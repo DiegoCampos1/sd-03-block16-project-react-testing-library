f0afbf1cb9dba5af974e8d8494e3f51fce95ab41
rsfdev <roger_faustino@hotmail.com>
2020-05-23 15:39:29 -0300

    Structure change

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,50 +1,31 @@
 import React from 'react';
-import renderWithRouter from '../renderWithRouter';
-import FavoritePokemons from '../components/FavoritePokemons';
-import data from '../data';
-import isPokemonFavoriteById from '../components/mocks';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { FavoritePokemons } from '../components';
+import App from '../App';
 
-const favoritePokemon = data.filter(({ id }) => isPokemonFavoriteById[id]);
-const notFavoritePokemon = data.filter(({ id }) => !isPokemonFavoriteById[id]);
+afterEach(cleanup);
 
-describe('FavoritePokemons.js', () => {
-  test('Favorite Pokemon', () => {
-    const { getByText } = renderWithRouter(
-      <FavoritePokemons pokemons={favoritePokemon} />,
-      { route: '/favorites' },
-    );
-    const heading = getByText('Favorite pokémons');
-    expect(heading).toBeInTheDocument();
+describe('FavoritePokemons.js testing', () => {
+  test('No favorite pokémon found text', () => {
+    const { getByText } = render(<FavoritePokemons pokemons={[]} />);
+    const notFavoritedPokemonText = getByText('No favorite pokemon found');
+    expect(notFavoritedPokemonText).toBeInTheDocument();
   });
 
-  test('notFound text in FavoritePokemons', () => {
-    const { getByText } = renderWithRouter(
-      <FavoritePokemons pokemons={[]} />,
-      { route: '/favorites' },
+  test('Pokémon favorite displayed', () => {
+    const { getAllByAltText, getByText, getByLabelText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
     );
-    const notFound = getByText('favorite pokemon not found');
-    expect(notFound).toBeInTheDocument();
-  });
-
-  test('favorite pokemon card', () => {
-    const { getByText, container } = renderWithRouter(
-      <FavoritePokemons pokemons={favoritePokemon} />,
-      { route: '/favorites' },
-    );
-    const cards = container.querySelectorAll('.favorite-pokemon');
-    expect(cards.length).toBe(5);
-    favoritePokemon.forEach(({ name }) => {
-      expect(getByText(name)).toBeInTheDocument();
-    });
-  });
-
-  test('favorite pokemon card > FavoritePokemons page', () => {
-    const { queryByText } = renderWithRouter(
-      <FavoritePokemons pokemons={favoritePokemon} />,
-      { route: '/favorites' },
-    );
-    notFavoritePokemon.forEach(({ name }) => {
-      expect(queryByText(name)).toBeNull();
-    });
+    const pokemonDetail = getByText(/More details/i);
+    fireEvent.click(pokemonDetail);
+    const favoriteButton = getByLabelText('Pokémon favoritado?');
+    fireEvent.click(favoriteButton);
+    const favoritePokemonButton = getByText('Favorite Pokémons');
+    fireEvent.click(favoritePokemonButton);
+    const pokemon = getAllByAltText(/is marked as favorite/i);
+    expect(pokemon.length).toBe(1);
   });
 });
