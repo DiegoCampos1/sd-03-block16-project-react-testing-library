a58d65d25a2a4f019bfe3c1969b2fa688248a926
luldelrey <rpgstrikered@gmail.com>
2020-05-22 22:18:58 -0300

    requisite 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,37 +1,25 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
-import App from '../App';
+import About from '../components/About';
 
 describe('Informações sobre a pokedex', () => {
   test('Contem um Header', () => {
-    const { getByText } = render(
-      <MemoryRouter initialEntries={['/about']}>
-        <App />
-      </MemoryRouter>,
-    );
+    const { getByText } = render(<About />);
 
     const heading = getByText(/About Pokédex/i);
     expect(heading).toBeInTheDocument();
   });
 
   test('Contem dois parágrafos', () => {
-    const { getAllByRole } = render(
-      <MemoryRouter initialEntries={['/about']}>
-        <App />
-      </MemoryRouter>,
-    );
+    const { getAllByRole } = render(<About />);
 
     const paragraphs = getAllByRole('region');
     expect(paragraphs).toHaveLength(2);
   });
 
   test('Contem imagem', () => {
-    const { getByRole } = render(
-      <MemoryRouter initialEntries={['/about']}>
-        <App />
-      </MemoryRouter>,
-    );
+    const { getByRole } = render(<About />);
 
     const image = getByRole('img');
     expect(image).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
