4a50449d7e416accdb478da619e9e82f39ed5684
lcdlima <leticia.duarte.lima@gmail.com>
2020-05-23 09:09:01 -0300

    Adjusting errors indicated by Code Climate

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { cleanup, render, fireEvent } from '@testing-library/react';
-import renderWithRouter from './renderWithRouter';
+import { cleanup, render } from '@testing-library/react';
 import FavoritePokemons from '../components/FavoritePokemons';
 
 afterEach(cleanup);
@@ -49,7 +48,7 @@ test('Dont display not favorite pokémons', () => {
   }];
   const { container } = render(
     <MemoryRouter>
-      <FavoritePokemons pokemons={pokemons}/>
+      <FavoritePokemons pokemons={pokemons} />
     </MemoryRouter>,
   );
 
@@ -58,38 +57,30 @@ test('Dont display not favorite pokémons', () => {
 
 test('If person has no favorite Pokémons', () => {
   const pokemons = [{
-    id: 4,
-    name: 'Charmander',
-    type: 'Fire',
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
     averageWeight: {
-      value: '8.5',
+      value: '6.0',
       measurementUnit: 'kg',
     },
-    image: 'https://cdn.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
-    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+    image: 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
     foundAt: [
       {
-        location: 'Alola Route 3',
-        map: 'https://cdn.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
       },
       {
-        location: 'Kanto Route 3',
-        map: 'https://cdn.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
-      },
-      {
-        location: 'Kanto Route 4',
-        map: 'https://cdn.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
-      },
-      {
-        location: 'Kanto Rock Tunnel',
-        map: 'https://cdn.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+        location: 'Kanto Power Plant',
+        map: 'https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
       },
     ],
-    summary: 'The flame on its tail shows the strength of its life force. If it is weak, the flame also burns weakly.',
+    summary: 'This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.',
   }];
   const { container } = render(
     <MemoryRouter>
-      <FavoritePokemons pokemons={pokemons}/>
+      <FavoritePokemons pokemons={pokemons} />
     </MemoryRouter>,
   );
 
