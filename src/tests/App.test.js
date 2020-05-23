f7357b2dd8d3d8e6825dd04808162317c1f0068b
lcdlima <leticia.duarte.lima@gmail.com>
2020-05-23 07:05:09 -0300

    Finishing all tests of App.test.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { cleanup, render, fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 afterEach(cleanup);
@@ -22,27 +23,48 @@ test('Fixed navigation links', () => {
     </MemoryRouter>,
   );
 
-  const homeElement = getByText('Home');
+  const homeElement = getByText(/Home/i);
   expect(homeElement.getAttribute("href")).toBe('/');
 
-  const aboutElement = getByText('About');
+  const aboutElement = getByText(/About/i);
   expect(aboutElement.getAttribute("href")).toBe('/about');
 
-  const favElement = getByText('Favorite Pokémons');
+  const favElement = getByText(/Favorite Pokémons/i);
   expect(favElement.getAttribute("href")).toBe('/favorites');
 })
 
 test('Click in Home', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  const { getByText, history } = renderWithRouter(<App />);
 
-  const homeElement = getByText('Home');
+  const homeElement = getByText(/Home/i);
   fireEvent.click(homeElement);
   const pathname = history.location.pathname;
   expect(pathname).toBe(`/`);
+})
+
+test('Click in About', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+
+  const aboutElement = getByText(/About/i);
+  fireEvent.click(aboutElement);
+  const pathname = history.location.pathname;
+  expect(pathname).toBe(`/about`);
+})
+
+test('Click in Favorites', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+
+  const favElement = getByText(/Favorite Pokémons/i);
+  fireEvent.click(favElement);
+  const pathname = history.location.pathname;
+  expect(pathname).toBe(`/favorites`);
+})
+ 
+test('Unknown page', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  history.push("/pagina/que-nao-existe/");
+    const noMatch = getByText(/Page requested not found/i);
+    expect(noMatch).toBeInTheDocument();
 })
 
 test('renders a reading with the text `Pokédex`', () => {
