b522d6ba1d9f366efdeaca581efd0eeadb6a63a0
lcdlima <leticia.duarte.lima@gmail.com>
2020-05-23 07:50:00 -0300

    Finishing all tests of About.test.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,47 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { cleanup, render } from '@testing-library/react';
+import About from '../components/About';
+
+afterEach(cleanup);
+
+test('Renders information about Pokédex', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
+  );
+
+  expect(getByText(/About Pokédex/i)).toBeInTheDocument();
+});
+
+test('Header must be h2 tag', () => {
+  const { getByRole } = render(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
+  );
+
+  expect(getByRole('heading', {level: 2})).toBeInTheDocument();
+});
+
+test('Check if page has 2 p tags', () => {
+  const { container } = render(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
+  );
+
+  expect(container.querySelectorAll('p').length).toBe(2);
+});
+
+test('Check source of image', () => {
+  const { getByRole } = render(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
+  );
+
+  const source = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+  expect(getByRole('img', {src: source})).toBeInTheDocument();
+});
