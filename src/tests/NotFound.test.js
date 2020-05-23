a6afbd995d3e151312a81878677dcd7b8b822f07
lcdlima <leticia.duarte.lima@gmail.com>
2020-05-23 09:44:20 -0300

    Finishing all tests of NotFound.test.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { cleanup, render, within } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+afterEach(cleanup);
+
+test('Header must be h2 tag', () => {
+  const { getByRole } = render(
+    <MemoryRouter>
+      <NotFound />
+    </MemoryRouter>,
+  );
+  
+  const { getByText } = within(getByRole('heading', { level: 2 }));
+  expect(getByText('Page requested not found')).toBeInTheDocument()  
+});
+
+test('Check source of image', () => {
+  const { getByAltText } = render(
+    <MemoryRouter>
+      <NotFound />
+    </MemoryRouter>,
+  );
+
+  const source = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  expect(getByAltText('Pikachu crying because the page requested was not found')).toHaveAttribute('src', source);
+});
