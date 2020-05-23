6439169e41ee9431eba84758b868244e0016b4b5
Gabriel <dreadpool38@gmail.com>
2020-05-23 17:42:11 -0300

    Pokemon tests

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import {
+  render, fireEvent,
+} from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import pokemons from '../data';
+import { Pokemon } from '../components';
+
+
+test('should ', () => {
+  const {
+    name, averageWeight: { value, measurementUnit }, image, id,
+  } = pokemons[0];
+  const history = createMemoryHistory();
+  const { getByText, getByTestId, getByRole } = render(
+    <Router history={history}>
+      <Pokemon pokemon={pokemons[0]} isFavorite={false} />
+    </Router>,
+  );
+  const poke = document.querySelector('.pokemon');
+  expect(poke).toBeInTheDocument();
+  const pName = getByTestId('pokemon-name');
+  expect(pName.textContent).toBe(name);
+
+  const peso = getByTestId('pokemon-weight');
+  expect(peso).toHaveTextContent(`Average weight:${value + measurementUnit}`);
+
+  const img = getByRole('img');
+  expect(img).toHaveAttribute('src', image);
+  expect(img).toHaveAttribute('alt', `${name} sprite`);
+
+  const link = getByRole('link');
+  expect(link).toHaveAttribute('href', `/pokemons/${id}`);
+  const button = getByText('More details');
+  console.log(button);
+
+  fireEvent.click(button);
+  expect(history.location.pathname).toBe(`/pokemons/${id}`);
+});
