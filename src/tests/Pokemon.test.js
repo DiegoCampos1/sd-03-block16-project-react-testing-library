5cf80b3409e047bba9721a1fdd993594bde7f654
lcdlima <leticia.duarte.lima@gmail.com>
2020-05-24 10:01:11 -0300

    Finishing all tests of Pokemon.test.js

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,54 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { cleanup, render, fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import data from '../data';
+import { Pokemon } from '../components';
+
+afterEach(cleanup);
+
+test('Correct Pokemon display', () => {
+  const { getByTestId } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByTestId('pokemon-name').innerHTML).toBe(data[0].name);
+  expect(getByTestId('pokemonType').innerHTML).toBe(data[0].type);
+  expect(getByTestId('pokemon-weight').innerHTML).toBe(`Average weight:` + `${data[0].averageWeight.value}` + `${data[0].averageWeight.measurementUnit}`);
+});
+
+test('Pokemon Image', () => {
+  const { getByAltText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const source = data[0].image;
+  expect(getByAltText(`${data[0].name} sprite`)).toHaveAttribute('src', source);
+});
+
+test('Details Link', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const detailsElement = getByText(/More details/i);
+  expect(detailsElement.getAttribute('href')).toBe(`/pokemons/${data[0].id}`);
+});
+
+test('Favorite Image', () => {
+  const { getByAltText } = render(
+    <MemoryRouter>
+      <Pokemon pokemon={data[0]}
+          isFavorite={true}/>
+    </MemoryRouter>,
+  );
+
+  expect(getByAltText(`${data[0].name} ` + `is marked as favorite`)).toHaveAttribute('src', `/star-icon.svg`);
+});
