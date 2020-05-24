0c9bc0f784a447317b9c3a170b75b1eedd325291
Gabryel <gabryel.ryba@gmail.com>
2020-05-24 16:09:11 -0300

    tests if App renders Pokedex main page when route path is '/'

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,3 +12,19 @@ test('renders a reading with the text `Pokédex`', () => {
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
+
+test('Renders Pokedex main page when route path is `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>,
+  );
+  const home = getByText('Home');
+  expect(home).toBeInTheDocument();
+
+  const about = getByText('About');
+  expect(about).toBeInTheDocument();
+
+  const favoritePokemons = getByText('Favorite Pokémons');
+  expect(favoritePokemons).toBeInTheDocument();
+});
