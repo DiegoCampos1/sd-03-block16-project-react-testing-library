50315bd2873c9e3c445224f143c8cf5b94a4a5dd
Gabryel <gabryel.ryba@gmail.com>
2020-05-24 18:43:16 -0300

    tests if NotFound component displays proper message and image

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('Not Found page shows not found message', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <NotFound />
+    </MemoryRouter>,
+  );
+  const notFoundMessage = getByText('Page requested not found 😭');
+  expect(notFoundMessage).toBeInTheDocument();
+});
