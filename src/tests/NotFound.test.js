ea182ca99272e654817a8215ad46bf433ae49334
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-05-24 14:59:21 -0300

    Requisito 4 finalizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+
+test('Testes do arquivo NotFound.js', () => {
+  const { getByText, getByAltText, history } = renderWithRouter(<NotFound />);
+  history.push('/paginaqualquer');
+  const msgNotFound = getByText('Page requested not found');
+  expect(msgNotFound).toBeInTheDocument();
+  const imgNotFound = getByAltText('Pikachu crying because the page requested was not found');
+  expect(imgNotFound).toBeInTheDocument();
+  expect(imgNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
