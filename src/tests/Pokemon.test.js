6c809a4c7965fc8ddc097a6f352bda55e620daff
William Igor <williamigorsilva@gmail.com>
2020-05-24 15:38:56 -0300

    Pokemon test

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,27 +1,56 @@
 import React from 'react';
+import { cleanup, getByAltText } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
+import Pokemon from '../components/Pokemon';
+import { pokemons } from './mock';
 
-test('Card Pokemons', () => {
-  const { getByText } = renderWithRouter(<App />);
-  const cardPokemon = getByText(/Encountered pokémons/i);
-  expect(cardPokemon).toBeInTheDocument();
-});
+afterEach(cleanup);
 
-test('Name pokemon in homepage', () => {
-  const { getByTestId } = renderWithRouter(<App />);
-  const pokemonName = getByTestId('pokemon-name');
-  expect(pokemonName).toBeInTheDocument();
-});
+describe('Return de information of one pokemon', () => {
+  test('Name pokemon in home page', () => {
+    const { getByTestId } = renderWithRouter((
+      <Pokemon pokemon={pokemons[0]} isFavorite={false} />));
+    const pokemonName = getByTestId('pokemon-name');
+    expect(pokemonName.textContent).toBe('Caterpie');
+  });
 
-// test('Test img in card', () => {
-// const { getByTestId } = renderWithRouter(<App />);
-// const img = getByTestId(`pokemons/${id}`);
-// expect(img).toBeInTheDocument();
-// });
+  test('Avarege weigth of the pokemon', () => {
+    const { getByText } = renderWithRouter((
+      <Pokemon pokemon={pokemons[0]} isFavorite={false} />));
+    const avaregeWeigth = getByText('Average weight:2.9kg');
+    expect(avaregeWeigth).toBeInTheDocument();
+  });
 
-test('Avarege weigth of the pokemon', () => {
-  const { getByTestId } = renderWithRouter(<App />);
-  const avaregeWeigth = getByTestId('pokemon-weight');
-  expect(avaregeWeigth).toBeInTheDocument();
+  test('Shold Image in pokemon', () => {
+    const { getByAltText } = renderWithRouter((
+      <Pokemon pokemon={pokemons[0]} isFavorite={false} />));
+    const img = getByAltText('Caterpie sprite');
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://cdn.bulbagarden.net/upload/8/83/Spr_5b_010.png');
+  });
+
+  test('Should id for pokemon witch link', () => {
+    const { getByText } = renderWithRouter(
+      <Pokemon pokemon={pokemons[0]} isFavorite={false} />);
+    const moreDetails = getByText('More details');
+    expect(moreDetails).toBeInTheDocument();
+    // expect(moreDetails.href).toBe('http://localhost/pokemons/10');
+    expect(moreDetails.href).toMatch('pokemons/10');
+  });
+
+  test('Should type of the pokemon', () => {
+    const { getByTestId } = renderWithRouter(
+      <Pokemon pokemon={pokemons[0]} isFavorite={false} />);
+    const typePokemon = getByTestId('pokemonType');
+    expect(typePokemon.textContent).toBe('Bug');
+  });
+
+  test('Should favorite icon with id', () => {
+    const { getByAltText } = renderWithRouter(
+      <Pokemon pokemon={pokemons[0]} isFavorite={true} />);
+    const img = getByAltText('Caterpie is marked as favorite');
+    expect(img).toBeInTheDocument();
+    expect(img.src).toMatch('/star-icon.svg');
+  });
 });
