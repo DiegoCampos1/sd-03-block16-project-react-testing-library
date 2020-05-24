472372d346724ed82b8c619aec4e09e36b706f1f
Gabryel <gabryel.ryba@gmail.com>
2020-05-24 18:46:56 -0300

    Notfound hotfix

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -9,6 +9,16 @@ test('Not Found page shows not found message', () => {
       <NotFound />
     </MemoryRouter>,
   );
-  const notFoundMessage = getByText('Page requested not found 😭');
+  const notFoundMessage = getByText('Page requested not found');
   expect(notFoundMessage).toBeInTheDocument();
+});
+
+test('Not Found page shows proper image', () => {
+  const { getByAltText } = render(
+    <MemoryRouter>
+      <NotFound />
+    </MemoryRouter>,
+  );
+  const pikachuImage = getByAltText('Pikachu crying because the page requested was not found');
+  expect(pikachuImage.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
 });
