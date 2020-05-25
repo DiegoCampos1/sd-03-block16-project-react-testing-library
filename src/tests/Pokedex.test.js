acb45fcf9a3d39e28a2bf3aabf7a8bcf7da48323
Marco Barbosa <marco.meireles.b@gmail.com>
2020-05-25 12:24:02 -0300

    Testes Pokedex criados

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,10 +2,22 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { cleanup, getNodeText, fireEvent } from '@testing-library/react';
 import App from '../App';
+import Pokedex from '../components/Pokedex';
 import pokemons from '../data';
 import renderWithRouter from './renderWithRouter';
 
 afterEach(cleanup);
+const favorites = {
+  4: false,
+  10: false,
+  23: false,
+  25: false,
+  65: false,
+  78: false,
+  143: false,
+  148: false,
+  151: false,
+};
 
 test('Test if button shows the next Pokemon', () => {
   const { getByText, getByTestId } = renderWithRouter(<App />);
@@ -34,7 +46,11 @@ test('Pokédex must show one pokemon per time', () => {
 });
 
 test('Pokedex must have filter buttons', () => {
-  const { getByTestId, getAllByRole, getAllByText } = renderWithRouter(<App />);
+  const { getAllByRole } = renderWithRouter(
+    <Pokedex
+    pokemons={pokemons} isPokemonFavoriteById={favorites}
+    />);
+
   const allButtons = [
     'All',
     'Electric',
@@ -53,18 +69,98 @@ test('Pokedex must have filter buttons', () => {
     expect(getNodeText(e)).toBe(allButtons[index]);
     return ('ok');
   });
+});
+
+test('Testing if filtered have displayed only filtered pokemon type', () => {
+  const { getByTestId, getAllByText } = renderWithRouter(
+    <Pokedex
+    pokemons={pokemons} isPokemonFavoriteById={favorites}
+    />);
+
+  const allButtons = [
+    'All',
+    'Electric',
+    'Fire',
+    'Bug',
+    'Poison',
+    'Psychic',
+    'Normal',
+    'Dragon',
+    'Próximo pokémon',
+  ];
 
   const filterButtons = allButtons.splice(1, 7);
 
-  filterButtons.map((e, index) => {
+  filterButtons.map((e) => {
     const actualButton = getAllByText(e);
+    let presentButton;
     if (actualButton.length === 2) {
-      fireEvent.click(actualButton[1]);
+      presentButton = actualButton[1];
+      fireEvent.click(presentButton);
     } else {
-      fireEvent.click(actualButton[0]);
+      presentButton = actualButton[0];
+      fireEvent.click(presentButton);
     }
-
+    const nextButton = getByTestId('next-pokemon');
+    const ativate = nextButton.disabled;
     const pokeType = getByTestId('pokemonType');
-    return expect(getNodeText(pokeType)).toBe(filterButtons[index]);
+    if(ativate) {
+      for (let i = 0; i < filterButtons.length; i += 1) {
+        fireEvent.click(presentButton);
+        const selectedType = getByTestId('pokemonType');
+        expect(pokeType).toBe(selectedType);
+      }
+    }
+  return ('ok');
   });
+});
+
+test('Testing reset filter button', () => {
+  const { getByTestId, getByText } = renderWithRouter(
+    <Pokedex
+    pokemons={pokemons} isPokemonFavoriteById={favorites}
+    />);
+  const resetButton = getByText('All');
+  expect(resetButton).toBeInTheDocument();
+  fireEvent.click(resetButton);
+
+  const button = getByText('Próximo pokémon');
+  let nextPokeName = '';
+  pokemons.map((e, index) => {
+    const poke = getByTestId('pokemon-name');
+    const pokeName = getNodeText(poke);
+    expect(pokeName).toBe(pokemons[index].name);
+    fireEvent.click(button);
+    const nextPoke = getByTestId('pokemon-name');
+    nextPokeName = getNodeText(nextPoke);
+    return ('ok');
+  });
+});
+
+test("Test if filter buttons are generate automatically", () => {
+  const { getAllByTestId } = renderWithRouter(
+    <Pokedex
+      pokemons={pokemons} isPokemonFavoriteById={favorites}
+    />);
+  let typesOfPokemons = [];
+  let uniqueTypesOfPokemons = [];
+  pokemons.map(e => {
+    typesOfPokemons.push(e.type);
+  });
+  uniqueTypesOfPokemons = [...new Set(typesOfPokemons)];
+  const button = getAllByTestId('pokemon-type-button');
+  expect(uniqueTypesOfPokemons.length).toBe(button.length);
+  button.map((e) => {
+    const buttonText = getNodeText(e);
+    expect(uniqueTypesOfPokemons.includes(buttonText)).toBe(true);
+  });
+});
+
+test('Test if renders title `Encountered pokémons`', () => {
+  const { getByRole } = renderWithRouter(
+    <Pokedex
+      pokemons={pokemons} isPokemonFavoriteById={favorites}
+    />);
+  const title = getByRole('heading');
+  expect(getNodeText(title)).toBe('Encountered pokémons');
 });
