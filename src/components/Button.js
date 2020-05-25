c14120d92bca9fc0c4af3e4e6e61440dd4dc5ffc
Wenderson belko <57719936+wendersonbelko@users.noreply.github.com>
2020-05-25 01:28:56 -0300

    commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,12 +8,14 @@ const Button = ({
   children,
   disabled,
   onClick,
+  dataTestId,
 }) => (
   <button
     onClick={onClick}
     className={`button-text ${className}`}
     disabled={disabled}
     type="button"
+    data-testid={dataTestId}
   >
     {children}
   </button>
@@ -24,12 +26,14 @@ Button.propTypes = {
   className: PropTypes.string,
   disabled: PropTypes.bool,
   onClick: PropTypes.func.isRequired,
+  dataTestId: PropTypes.string,
 };
 
 Button.defaultProps = {
   children: null,
   className: '',
   disabled: false,
+  dataTestId: undefined,
 };
 
 export default Button;
--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { render, cleanup } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+afterEach(cleanup);
+
+function renderWithRouter(
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+describe('No topo da aplicação, deve haver um conjunto fixo de links de navegação', () => {
+  it('A página "About" deve exibir informações sobre a Pokédex', () => {
+    const { getByText, getByAltText } = renderWithRouter(<App />, { route: '/About' });
+    const about = getByText(/About Pokédex/i);
+    expect(about).toBeInTheDocument();
+    const info = getByText(/This application simulates a Pokédex, a digital encliclopedia containing all Pokémons/i);
+    expect(info).toBeInTheDocument();
+    const secondInfo = getByText(/One can filter Pokémons by type, and see more details for each one of them/i);
+    expect(secondInfo).toBeInTheDocument();
+
+    const img = getByAltText(/Pokédex/i);
+    const imgSrc = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(img.src).toEqual(imgSrc);
+  });
+});
