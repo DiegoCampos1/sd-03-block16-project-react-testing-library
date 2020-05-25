f3be10355d023b8b51231e6e5939e2b5bd76a952
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-05-25 01:39:12 -0300

    Requisito 6 finalizado

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -51,7 +51,7 @@ class Pokedex extends React.Component {
         </Button>
         {pokemonTypes.map((type) => (
           <Button
-            dataTestId={`pokemon-type-button`}
+            /* dataTestId={`pokemon-type-button`} */
             key={type}
             onClick={() => this.filterPokemons(type)}
             className="filter-button"
