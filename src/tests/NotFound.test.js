d353154c1c46726b37c9dda5925e9c0fb6cce267
Rhian Castro <rhi.castro@gmail.com>
2020-05-26 16:16:10 -0300

    Not Found test

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('NotFound.js component tests', () => {
+  test('NotFound page renders Pokédex information in heading element', () => {
+    const { getByRole, getByText, getByLabelText } = render(<NotFound />);
+    const heading = getByRole('heading');
+    const headingEmoji = getByLabelText('Crying emoji');
+    const headingText = getByText('Page requested not found');
+    expect(heading).toBeInTheDocument();
+    expect(headingEmoji).toBeInTheDocument();
+    expect(headingText).toBeInTheDocument();
+    expect(heading.tagName).toBe('H2');
+  });
+
+  test('NotFound page render img element with correct src', () => {
+    const { getByAltText } = render(<NotFound />);
+    const img = getByAltText(/Pikachu crying because the page requested was not found/i);
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
