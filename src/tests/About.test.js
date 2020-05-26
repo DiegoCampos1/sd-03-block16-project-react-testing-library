e2c9e991e3b55758f79ed22776e97d03b8bb61ba
tamagoshii <estelaap_martins@hotmail.com>
2020-05-26 17:10:24 -0300

    teste

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+test('', () => {
+  const { getByText, getByRole } = render(
+    <MemoryRouter initialEntries={[{ pathname: '/about' }]}>
+      <App />
+    </MemoryRouter>,
+  );
+  expect(getByText('About Pokédex')).toBeInTheDocument();
+  const n1 = getByText(/Simula um Pokédex,/);
+  const n2 = getByText(/filtra Pokémons por tipo/);
+
+  expect(n1).toBeInTheDocument();
+
+  expect(n2).toBeInTheDocument();
+
+  const imagem = getByRole('img');
+  expect(imagem.src).toMatch('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,33 +1,47 @@
 import React from 'react';
-import { cleanup } from '@testing-library/react';
-import renderWithRouter from '../renderWithRouter';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
 
-afterEach(cleanup);
+test('renders a reading with the text `Pokédex`', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const heading = getByText(/Pokédex/i);
+  expect(heading).toBeInTheDocument();
+});
 
-describe('Test 1 - about page should display info about pokedex', () => {
-  it('1 - heading <h2> with text about pokedex', () => {
-    const { queryByText } = renderWithRouter(<App />, { route: '/about' });
-    const heading = queryByText(/About Pokédex/i);
-    expect(heading).toBeInTheDocument();
-    expect(heading.tagName).toBe('H2');
-  });
-  it('1.2 - two paragraphs with text pokedex', () => {
-    const { queryByText } = renderWithRouter(<App />, { route: '/about' });
-    const heading = queryByText(/About Pokédex/i);
-    expect(heading.tagName).toBe('H2');
-    const description = heading.nextSibling;
-    let tagPLength = 0;
-    description.childNodes.forEach((element) => {
-      if (element.tagName === 'P') {
-        tagPLength += 1;
-        expect(element.tagName).toBe('P');
-      }
-    });
-    expect(tagPLength).toEqual(2);
-  });
-  it('1.3 - should have right image path', () => {
-    const { getByAltText } = renderWithRouter(<App />, { route: '/about' });
-    expect(getByAltText(/Pokédex/i).src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
-  });
+test('aplicação deve ser redirecionada para a página inicial', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const homeLink = getByText('Home');
+  fireEvent.click(homeLink);
+  expect(getByText(/Encountered pokémons/i));
+});
+
+test('aplicação deve ser redirecionada para a página de About', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const aboutLink = getByText(/About/);
+  fireEvent.click(aboutLink);
+  expect(getByText(/About Pokédex/i));
+});
+
+test('aplicação deve ser redirecionada para a página de pokémons favoritados', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const favoritesLink = getByText(/Favorite Pokémons/);
+  fireEvent.click(favoritesLink);
+  expect(getByText(/Favorite pokémons/));
 });
