bc44c4fb28cd995b6bf1d3ad9a03f183415db7cb
tamagoshii <estelaap_martins@hotmail.com>
2020-05-26 18:54:22 -0300

    validando req 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+
+import App from '../App';
+import pokemons from '../data';
+
+describe('Teste Pokemon', () => {
+  pokemons.forEach(({
+    name, id, summary, foundAt,
+  }) => {
+    test(`pokemon ${name} name, Summary`, () => {
+      const {
+        getByText, getAllByAltText,
+      } = render(
+        <MemoryRouter initialEntries={[{ pathname: `/pokemons/${id}` }]}>
+          <App />
+        </MemoryRouter>,
+      );
+      expect(getByText(`${name} Details`)).toBeInTheDocument();
+      expect(getByText('Summary')).toBeInTheDocument();
+      expect(getByText(summary)).toBeInTheDocument();
+      expect(getByText(`Game Locations of ${name}`)).toBeInTheDocument();
+      const localImagem = getAllByAltText(`${name} location`);
+      const favoritos = getByText('Pokémon favoritado?');
+      fireEvent.click(favoritos);
+      fireEvent.click(favoritos);
+      console.log(favoritos.innerHTML);
+      foundAt.forEach(({ location, map }) => {
+        expect(getByText(location)).toBeInTheDocument();
+        expect(localImagem.some(({ src }) => src === map)).toBeTruthy();
+      });
+    });
+  });
+});
