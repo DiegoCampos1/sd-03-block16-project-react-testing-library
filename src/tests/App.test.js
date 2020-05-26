67fd3960493b15fa0bd3c1d898ebbf7a7d7e9a6e
tamagoshii <estelaap_martins@hotmail.com>
2020-05-26 16:21:07 -0300

    validando requisito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { cleanup } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
+afterEach(cleanup);
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+describe('Test 1 - about page should display info about pokedex', () => {
+  it('1 - heading <h2> with text about pokedex', () => {
+    const { queryByText } = renderWithRouter(<App />, { route: '/about' });
+    const heading = queryByText(/About Pokédex/i);
+    expect(heading).toBeInTheDocument();
+    expect(heading.tagName).toBe('H2');
+  });
+  it('1.2 - two paragraphs with text pokedex', () => {
+    const { queryByText } = renderWithRouter(<App />, { route: '/about' });
+    const heading = queryByText(/About Pokédex/i);
+    expect(heading.tagName).toBe('H2');
+    const description = heading.nextSibling;
+    let tagPLength = 0;
+    description.childNodes.forEach((element) => {
+      if (element.tagName === 'P') {
+        tagPLength += 1;
+        expect(element.tagName).toBe('P');
+      }
+    });
+    expect(tagPLength).toEqual(2);
+  });
+  it('1.3 - should have right image path', () => {
+    const { getByAltText } = renderWithRouter(<App />, { route: '/about' });
+    expect(getByAltText(/Pokédex/i).src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
