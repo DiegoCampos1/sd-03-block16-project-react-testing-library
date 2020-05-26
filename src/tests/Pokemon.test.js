7ed1c12a43a7439cad73620c123dcf72919e3f21
tamagoshii <estelaap_martins@hotmail.com>
2020-05-26 18:46:59 -0300

    validando req 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,72 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testando informações dos Cards Pokemon', () => {
+  pokemons.forEach(({
+    name, averageWeight: { value, measurementUnit }, image, type,
+  }, index) => {
+    test(`pokemon ${name} Card test`, async () => {
+      const {
+        getByText, getByAltText, getByTestId,
+      } = render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+
+      if (index === pokemons.length - 1) return null;
+      Array(index).fill(index).forEach(() => fireEvent.click(getByText(/próximo pokémon/i)));
+
+      const testeCard = () => {
+        expect(getByText(name)).toBeInTheDocument();
+        expect(getByTestId('pokemonType').innerHTML).toBe(type);
+        expect(getByText(`Average weight:${value}${measurementUnit}`)).toBeInTheDocument();
+        const imagem = getByAltText(`${name} sprite`);
+        expect(imagem.alt).toMatch(`${name} sprite`);
+        expect(imagem.src).toMatch(image);
+      };
+      return testeCard();
+    });
+  });
+
+  pokemons.forEach(({ name, id }, index) => {
+    test(`Detalhes de ${name}`, () => {
+      let localMock;
+      const {
+        getByText, getByAltText, getByRole,
+      } = render(
+        <MemoryRouter>
+          <App />
+          <Route
+            path="*"
+            render={({ location }) => {
+              localMock = location;
+              return null;
+            }}
+          />
+        </MemoryRouter>,
+      );
+
+      Array(index).fill(index).forEach(() => fireEvent.click(getByText(/Próximo pokémon/i)));
+      const testDetails = () => {
+        const detailsLink = getByText('More details');
+        fireEvent.click(detailsLink);
+        expect(localMock.pathname.split('/pokemons/')[1]).toBe(id.toString());
+
+        const favoriteCheckbox = getByRole('checkbox');
+        fireEvent.click(favoriteCheckbox);
+
+        const starIcon = getByAltText(`${name} is marked as favorite`);
+        expect(starIcon).toBeInTheDocument();
+        expect(starIcon.src).toMatch('/star-icon.svg');
+        // const homeLink = getByText('Home');
+      };
+
+      return testDetails();
+    });
+  });
+});
