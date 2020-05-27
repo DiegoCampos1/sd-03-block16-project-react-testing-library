609ab7d773273e7c4236b313bde425b633597eaa
mirellaalves <mirella.alves@gmail.com>
2020-05-27 12:08:53 -0300

    Testing image on About page.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+test('A página deve conter a seguinte imagem de uma Pokédex: https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.', () => {
+  const { queryByAltText } = render(<About />);
+  const pokedex = queryByAltText('Pokédex');
+  expect(pokedex.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
