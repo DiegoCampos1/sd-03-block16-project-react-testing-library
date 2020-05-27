a4ce2027b7c948b258a376b42b058235a1707409
mirellaalves <mirella.alves@gmail.com>
2020-05-27 15:18:22 -0300

    Code climate issues.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -5,7 +5,7 @@ import About from '../components/About';
 test('A página deve conter um heading h2 com o texto About Pokédex.', () => {
   const { queryAllByRole, getByText } = render(<About />);
   const headH2 = queryAllByRole('heading');
-  const headH2Text = getByText('About Pokédex')
+  const headH2Text = getByText('About Pokédex');
   expect(headH2).toHaveLength(1);
   expect(headH2Text).toBeInTheDocument();
 });
