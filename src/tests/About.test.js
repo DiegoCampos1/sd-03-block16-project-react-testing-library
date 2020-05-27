919c0ce568cc208ad0ef3605629c53e4c6e19166
Clayton Pereira <cpereiramt@gmail.com>
2020-05-27 17:09:30 -0400

    Salvando alteraçoes no requisito about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { cleanup, render } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
-import '@testing-library/jest-dom/extend-expect';
+// import { getByAltText } from '@testing-library/jest-dom/extend-expect';
 import About from '../components/About';
 
 afterEach(cleanup);
@@ -22,4 +22,12 @@ test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = renderWithRouter(<About />);
   const about = getByText(/About Pokédex/i);
   expect(about).toBeInTheDocument();
+});
+
+test('check if renders image', () => {
+  const { getByAltText } = renderWithRouter(<About />);
+  const pokedeximg = getByAltText('Pokédex');
+  expect(pokedeximg).toBeInTheDocument();
+  const img = pokedeximg.src;
+  expect(img).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 });
