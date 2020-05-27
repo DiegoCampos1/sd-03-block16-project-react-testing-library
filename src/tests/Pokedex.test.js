adcf708332bf36836edcf89b73de68408a682e37
Clayton Pereira <cpereiramt@gmail.com>
2020-05-27 18:57:26 -0400

    Salvando alteraçoes no requisito Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { cleanup, render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+// import { getByAltText } from '@testing-library/jest-dom/extend-expect';
+import App from '../App';
+
+afterEach(cleanup);
+
+function renderWithRouter(
+  ui,
+  { route = '/asdfasdf', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+
+test('renders a notfound Page`', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const nextButton = getByText('Próximo pokémon');
+  expect(nextButton).toBeInTheDocument();
+});
