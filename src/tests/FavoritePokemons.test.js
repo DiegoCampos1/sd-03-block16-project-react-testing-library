229f94825405b4668405de0b18abd082333279fb
Clayton Pereira <cpereiramt@gmail.com>
2020-05-27 17:41:11 -0400

    Salvando alteraçoes no requisito FavoritePokemon

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -17,8 +17,23 @@ function renderWithRouter(
   };
 }
 
-test('Testing Favorite Pokemon Page', () => {
+test('Testing Favorite Pokemon Page to be emptly', () => {
   const { getByText } = renderWithRouter(<FavoritePokemons pokemons={[]} />);
   const emptly = getByText(/No favorite pokemon found/i);
   expect(emptly).toBeInTheDocument();
+});
+
+test('Testing rendering favorite pokemon', () => {
+  const { getByTestId } = renderWithRouter(<FavoritePokemons pokemons={[{
+    id: 10,
+    name: 'Caterpie',
+    type: 'Bug',
+    averageWeight: {
+      value: '2.9',
+      measurementUnit: 'kg',
+    },
+  }]}
+  />);
+  const withElements = getByTestId(/pokemon-name/i);
+  expect(withElements).toBeInTheDocument();
 });
