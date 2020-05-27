2423a63995f74a3e29309b9c3df3307ec84d8c0e
Clayton Pereira <cpereiramt@gmail.com>
2020-05-27 18:24:33 -0400

    Salvando alteraçoes no requisito Notfound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { cleanup, render, getAllByAltText } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+// import { getByAltText } from '@testing-library/jest-dom/extend-expect';
+import NotFound from '../components/NotFound';
+
+afterEach(cleanup);
+
+function renderWithRouter(
+  ui,
+  { route = '/about', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+
+test('renders a notfound Page`', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+  const N404 = getByText('Page requested not found');
+  expect(N404).toBeInTheDocument();
+});
+
+test('check if renders image', () => {
+  const { getByAltText } = renderWithRouter(<NotFound />);
+  const pikachuCrying = getByAltText('Pikachu crying because the page requested was not found');
+  expect(pikachuCrying).toBeInTheDocument();
+  const img = pikachuCrying.src;
+  expect(img).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
