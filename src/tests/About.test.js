36ec61f016bfaee7eef01a3f4cb49b8d2024715a
mirellaalves <mirella.alves@gmail.com>
2020-05-27 13:35:31 -0300

    Testing paragraphs on About page.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, getByDisplayValue } from '@testing-library/react';
 import About from '../components/About';
+
+test('A página deve conter dois parágrafos com texto sobre a Pokédex.', () => {
+  const { queryAllByRole } = render(<About />);
+  const paragraph = queryAllByRole('region');
+  expect(paragraph).toHaveLength(2);
+  expect(paragraph).not.toBeNull();
+});
 
 test('A página deve conter a seguinte imagem de uma Pokédex: https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.', () => {
   const { queryByAltText } = render(<About />);
