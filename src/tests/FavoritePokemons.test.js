bdb981b051e3b80243be521ee77e8d4f51a8efc7
Clayton Pereira <cpereiramt@gmail.com>
2020-05-27 17:30:22 -0400

    Salvando alteraçoes no requisito FavoritePokemon

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { cleanup, render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+// import { getByAltText } from '@testing-library/jest-dom/extend-expect';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+afterEach(cleanup);
+
+function renderWithRouter(
+  ui,
+  { route = '/about', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+test('Testing Favorite Pokemon Page', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons pokemons={[]} />);
+  const emptly = getByText(/No favorite pokemon found/i);
+  expect(emptly).toBeInTheDocument();
+});
