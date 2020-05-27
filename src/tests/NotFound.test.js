64e16aef625c4f1e5cc4b43a35792d1d32714899
Clayton Pereira <cpereiramt@gmail.com>
2020-05-27 18:31:38 -0400

    Salvando alteraçoes no requisito Notfound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-import { cleanup, render, getAllByAltText } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 // import { getByAltText } from '@testing-library/jest-dom/extend-expect';
 import NotFound from '../components/NotFound';
@@ -9,7 +9,7 @@ afterEach(cleanup);
 
 function renderWithRouter(
   ui,
-  { route = '/about', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+  { route = '/asdfasdf', history = createMemoryHistory({ initialEntries: [route] }) } = {},
 ) {
   return {
     ...render(<Router history={history}>{ui}</Router>),
