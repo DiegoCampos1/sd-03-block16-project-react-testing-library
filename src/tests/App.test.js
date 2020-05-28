112f12e83883130406b96841460fb8534a0bf52a
Cardoso <lsc.cardosoo@gmail.com>
2020-05-28 15:56:58 -0300

    renderwithrouter adicionado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,21 +1,8 @@
 import React from 'react';
-import { MemoryRouter, Router } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { render, fireEvent, cleanup } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
-
-function renderWithRouter(
-  ui,
-  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
-) {
-  return {
-    ...render(<Router history={history}>{ui}</Router>),
-    // adding `history` to the returned utilities to allow us
-    // to reference it in our tests (just try to avoid using
-    // this to test implementation details).
-    history,
-  };
-}
 
 describe('Routes', () => {
   afterEach(cleanup);
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
+
+describe('', () => {
+  afterEach(cleanup);
+  const isPokemonFavoriteById = {
+    25: false,
+    4: false,
+    10: false,
+    23: false,
+    65: false,
+    151: false,
+    78: false,
+    143: false,
+    148: false,
+  };
+  test('', () => {
+    const { getAllByTestId } = render(<Pokedex
+      pokemons={pokemons}
+      isPokemonFavoriteById={isPokemonFavoriteById}
+    />);
+  });
+})
