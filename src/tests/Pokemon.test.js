b07480f1e31b74f3af7a94be66dbe3706c4fdd09
Clayton Guimaraes Leite Pereira <claytonpereira@mtprev.local>
2020-05-28 09:54:50 -0400

    Salvando requisito Pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { cleanup, render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+// import { getByAltText } from '@testing-library/jest-dom/extend-expect';
+import Pokemon from '../components/Pokemon';
+import pokemons from '../data';
+
+afterEach(cleanup);
+
+function renderWithRouter(
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+test('Renders Pokemon Cards', () => {
+  const { queryByTestId, getByText } = renderWithRouter(
+    <Pokemon
+      pokemon={pokemons[2]}
+      showDetailsLink
+      isFavorite
+    />
+  );
+  const pokemonName = queryByTestId('pokemon-name');
+  expect(pokemonName).toBeInTheDocument();
+  console.log(pokemonName.innerHTML);
+  const pokemonH2Name = getByText('Caterpie');
+  expect(pokemonH2Name).toBeInTheDocument();
+});
