229ed441b57e3a410ceaad3f69cb1ce80b158efc
mirellaalves <mirella.alves@gmail.com>
2020-05-29 00:56:22 -0300

    Testing pokemon link detail and favorite icon in Pokemon component.

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import renderWithRouter from '../services/renderWithRouter';
 import App from '../App';
 
@@ -15,8 +15,23 @@ test('O peso médio do pokémon deve ser exibido.', () => {
   expect(pokemonWeight.textContent).toBe('Average weight:6.0kg');
 });
 
-test('A imagem deve conter um atributo src com a URL da imagem do pokémon e um atributo alt com o texto <name> sprite, onde <name> é o nome do pokémon.', () => {
+test('A imagem deve conter um atributo src com a URL da imagem e alt com o texto <name> sprite.', () => {
   const { queryByAltText } = renderWithRouter(<App />);
   const pokemonImg = queryByAltText('Pikachu sprite');
   expect(pokemonImg.src).toBe('https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+});
+
+test('Deve conter um link de navegação para exibir detalhes.', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/More details/i));
+  const pathname = history.location.pathname;
+  expect(pathname).toBe('/pokemons/25');
+});
+
+test('Pokémons favoritados devem exibir um ícone de uma estrela.', () => {
+  const { getByText, queryByAltText } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/More details/i));
+  fireEvent.click(getByText(/Pokémon favoritado?/i));
+  const favoriteIcon = queryByAltText('Pikachu is marked as favorite');
+  expect(favoriteIcon.src).toBe('http://localhost/star-icon.svg');
 });
