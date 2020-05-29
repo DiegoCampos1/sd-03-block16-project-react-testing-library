e6da0aabb69b102e0ab9071699064c1010887ea6
mirellaalves <mirella.alves@gmail.com>
2020-05-29 03:06:27 -0300

    Testing next pokemon button in Pokedex component.

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+test('Ao apertar o botão de próximo, a página deve exibir o próximo pokémon da lista', () => {
+  const { getByText } = renderWithRouter(<App />);
+
+  const nextButton = getByText('Próximo pokémon');
+  expect(nextButton).toBeInTheDocument();
+
+  fireEvent.click(getByText(/Próximo pokémon/i));
+  const nextPokemon1 = getByText('Charmander');
+  expect(nextPokemon1).toBeInTheDocument();
+
+  fireEvent.click(getByText(/Próximo pokémon/i));
+  const nextPokemon2 = getByText('Caterpie');
+  expect(nextPokemon2).toBeInTheDocument();
+});
