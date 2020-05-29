ce69265120f75f659921e2ac8160f00e0ae0341b
mirellaalves <mirella.alves@gmail.com>
2020-05-28 23:54:15 -0300

    Testing pokemon name and pokemon weight in Pokemon component.

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+// import { render } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+test('O nome correto do pokémon deve aparecer na tela.', () => {
+  const { getByTestId } = renderWithRouter(<App />);
+  const pokemonName = getByTestId('pokemon-name');
+  expect(pokemonName.textContent).toBe('Pikachu');
+});
+
+test('O peso médio do pokémon deve ser exibido.', () => {
+  const { getByTestId } = renderWithRouter(<App />);
+  const pokemonWeight = getByTestId('pokemon-weight');
+  expect(pokemonWeight.textContent).toBe('Average weight:6.0kg');
+});
