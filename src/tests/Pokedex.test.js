7dcf30e76650c796d9838ffdfa1d12a02cd1be42
Gustavo <ggteodor07@gmai.com>
2020-05-29 01:19:32 -0300

    Third try

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,11 +1,211 @@
 import React from 'react';
 import { fireEvent, cleanup } from '@testing-library/react';
-import pokemons from '../data';
 import Pokedex from '../components/Pokedex';
 import App from '../App';
 import renderWithRouter from '../services/renderWithRouter';
 
-const typeButtons = () => pokemons.reduce((acc, e) => {
+const mockPokemons = [
+  {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: 'This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.',
+  },
+  {
+    id: 4,
+    name: 'Charmander',
+    type: 'Fire',
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Alola Route 3',
+        map: 'https://cdn.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 3',
+        map: 'https://cdn.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 4',
+        map: 'https://cdn.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+      },
+      {
+        location: 'Kanto Rock Tunnel',
+        map: 'https://cdn.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+      },
+    ],
+    summary: 'The flame on its tail shows the strength of its life force. If it is weak, the flame also burns weakly.',
+  },
+  {
+    id: 10,
+    name: 'Caterpie',
+    type: 'Bug',
+    averageWeight: {
+      value: '2.9',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn.bulbagarden.net/upload/8/83/Spr_5b_010.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Johto Route 30',
+        map: 'https://cdn.bulbagarden.net/upload/7/76/Johto_Route_30_Map.png',
+      },
+      {
+        location: 'Johto Route 31',
+        map: 'https://cdn.bulbagarden.net/upload/2/2b/Johto_Route_31_Map.png',
+      },
+      {
+        location: 'Ilex Forest',
+        map: 'https://cdn.bulbagarden.net/upload/a/ae/Johto_Ilex_Forest_Map.png',
+      },
+      {
+        location: 'Johto National Park',
+        map: 'https://cdn.bulbagarden.net/upload/4/4e/Johto_National_Park_Map.png',
+      },
+    ],
+    summary: 'For protection, it releases a horrible stench from the antennae on its head to drive away enemies.',
+  },
+  {
+    id: 23,
+    name: 'Ekans',
+    type: 'Poison',
+    averageWeight: {
+      value: '6.9',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn.bulbagarden.net/upload/1/18/Spr_5b_023.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Ekans_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Goldenrod Game Corner',
+        map: 'https://cdn.bulbagarden.net/upload/e/ec/Johto_Goldenrod_City_Map.png',
+      },
+    ],
+    summary: 'It can freely detach its jaw to swallow large prey whole. It can become too heavy to move, however.',
+  },
+  {
+    id: 65,
+    name: 'Alakazam',
+    type: 'Psychic',
+    averageWeight: {
+      value: '48.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn.bulbagarden.net/upload/8/88/Spr_5b_065_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Alakazam_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Unova Accumula Town',
+        map: 'https://cdn.bulbagarden.net/upload/4/44/Unova_Accumula_Town_Map.png',
+      },
+    ],
+    summary: 'Closing both its eyes heightens all its other senses. This enables it to use its abilities to their extremes.',
+  },
+  {
+    id: 151,
+    name: 'Mew',
+    type: 'Psychic',
+    averageWeight: {
+      value: '4.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn.bulbagarden.net/upload/4/43/Spr_5b_151.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Mew_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Faraway Island',
+        map: 'https://cdn.bulbagarden.net/upload/e/e4/Hoenn_Faraway_Island_Map.png',
+      },
+    ],
+    summary: 'Apparently, it appears only to those people who are pure of heart and have a strong desire to see it.',
+  },
+  {
+    id: 78,
+    name: 'Rapidash',
+    type: 'Fire',
+    averageWeight: {
+      value: '95.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn.bulbagarden.net/upload/5/58/Spr_5b_078.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Rapidash_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Route 28',
+        map: 'https://cdn.bulbagarden.net/upload/5/5b/Kanto_Route_28_Map.png',
+      },
+      {
+        location: 'Johto Mount Silver',
+        map: 'https://cdn.bulbagarden.net/upload/9/95/Johto_Mt_Silver_Map.png',
+      },
+    ],
+    summary: 'At full gallop, its four hooves barely touch the ground because it moves so incredibly fast.',
+  },
+  {
+    id: 143,
+    name: 'Snorlax',
+    type: 'Normal',
+    averageWeight: {
+      value: '460.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn.bulbagarden.net/upload/4/40/Spr_5b_143.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Snorlax_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Vermillion City',
+        map: 'https://cdn.bulbagarden.net/upload/5/54/Kanto_Vermilion_City_Map.png',
+      },
+    ],
+    summary: 'What sounds like its cry may actually be its snores or the rumblings of its hungry belly.',
+  },
+  {
+    id: 148,
+    name: 'Dragonair',
+    type: 'Dragon',
+    averageWeight: {
+      value: '16.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn.bulbagarden.net/upload/2/2c/Spr_5b_148.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Dragonair_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Johto Route 45',
+        map: 'https://cdn.bulbagarden.net/upload/2/21/Johto_Route_45_Map.png',
+      },
+      {
+        location: 'Johto Dragon\'s Den',
+        map: 'https://cdn.bulbagarden.net/upload/1/1e/Johto_Dragons_Den_Map.png',
+      },
+    ],
+    summary: 'They say that if it emits an aura from its whole body, the weather will begin to change instantly.',
+  },
+];
+
+const typeButtons = () => mockPokemons.reduce((acc, e) => {
   if (acc.includes(e.type)) {
     return acc;
   }
@@ -33,7 +233,7 @@ test('Testing the reset of the next pokemon button ', () => {
   const nextButton = getByTestId(/next-pokemon/i);
   expect(nextButton).toBeInTheDocument();
   expect(nextButton).toHaveTextContent(/Próximo pokémon/i);
-  pokemons.forEach((elem) => {
+  mockPokemons.forEach((elem) => {
     const pokemonName = getByTestId(/pokemon-name/i);
     const pokemonType = getByTestId(/pokemonType/i);
     const pokemonWeight = getByTestId(/pokemon-weight/i);
@@ -46,7 +246,7 @@ test('Testing the reset of the next pokemon button ', () => {
 
 test('Testing just one pokémon on display ', () => {
   const { queryAllByTestId } = renderWithRouter(<Pokedex
-    pokemons={pokemons}
+    pokemons={mockPokemons}
     isPokemonFavoriteById={mockIsPokemonFavoriteById}
   />);
   const displayOfPokemons = queryAllByTestId(/pokemon-name/i);
@@ -55,7 +255,7 @@ test('Testing just one pokémon on display ', () => {
 
 test('Testing the type buttons ', () => {
   const { getAllByTestId } = renderWithRouter(<Pokedex
-    pokemons={pokemons}
+    pokemons={mockPokemons}
     isPokemonFavoriteById={mockIsPokemonFavoriteById}
   />);
   const pokemonTypeButton = getAllByTestId(/pokemon-type-button/i);
@@ -66,7 +266,7 @@ test('Testing the type buttons ', () => {
 
 test('Testing the reset button all ', () => {
   const { getByText, getByTestId, getAllByTestId } = renderWithRouter(<Pokedex
-    pokemons={pokemons}
+    pokemons={mockPokemons}
     isPokemonFavoriteById={mockIsPokemonFavoriteById}
   />);
   const allButton = getByText(/All/i);
@@ -81,7 +281,7 @@ test('Testing the reset button all ', () => {
 
 test('Testing the disable next button ', () => {
   const { getByTestId, getAllByTestId } = renderWithRouter(<Pokedex
-    pokemons={pokemons}
+    pokemons={mockPokemons}
     isPokemonFavoriteById={mockIsPokemonFavoriteById}
   />);
   const nextButton = getByTestId(/next-pokemon/i);
