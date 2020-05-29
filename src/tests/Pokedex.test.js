0b91a2b2d25796a6fe514c47d1875e2519d18afc
renan.andreolle <renandreolle@hotmail.com>
2020-05-29 11:46:37 -0300

    Metade da pokedex feita

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, getAllByTestId } from '@testing-library/react';
+
+import MemoryHistory from './MemoryHistory';
+import Pokedex from '../components/Pokedex';
+
+
+describe('Testando a pokedex', () => {
+  it('Testando o botão de próximo', () => {
+    const {getAllByTestId, getByText } = MemoryHistory(<Pokedex />);
+    const prox = getByText('Próximo pokemon');
+    const type = getAllByTestId('pokemon-type-button');
+    const encontrado = getByText('Encountered pokémons');
+    const all = getByText('All');
+    expect(prox).toBeInTheDocument();
+    expect(type[0]).toBeInTheDocument();
+    expect(encontrado).toBeInTheDocument();
+    expect(all).toBeInTheDocument();
+  });
+});
