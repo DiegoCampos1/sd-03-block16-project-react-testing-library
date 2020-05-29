2a8178bcbbe6602cdf8615598b78000e714b20f2
renan.andreolle <renandreolle@hotmail.com>
2020-05-29 10:08:37 -0300

    Not Found found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import MemoryHistory from './MemoryHistory';
+import NotFound from '../components/NotFound';
+
+describe('Testando o NotFound', () => {
+  it('Testando o texto', () => {
+    const { getByText } = MemoryHistory(<NotFound />);
+    const notFoundText = getByText('Page requested not found');
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  it('Testando a imagem', () => {
+    const { getByAltText } = MemoryHistory(<NotFound />);
+    const foto = getByAltText('Pikachu crying because the page requested was not found');
+    expect(foto).toBeInTheDocument();
+  });
+})
