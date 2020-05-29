f1dea4caef39c270dff614769334f6c40f76f68f
Gustavo <ggteodor07@gmai.com>
2020-05-29 01:27:34 -0300

    Fourth try

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -124,46 +124,6 @@ const mockPokemons = [
     summary: 'Closing both its eyes heightens all its other senses. This enables it to use its abilities to their extremes.',
   },
   {
-    id: 151,
-    name: 'Mew',
-    type: 'Psychic',
-    averageWeight: {
-      value: '4.0',
-      measurementUnit: 'kg',
-    },
-    image: 'https://cdn.bulbagarden.net/upload/4/43/Spr_5b_151.png',
-    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Mew_(Pok%C3%A9mon)',
-    foundAt: [
-      {
-        location: 'Faraway Island',
-        map: 'https://cdn.bulbagarden.net/upload/e/e4/Hoenn_Faraway_Island_Map.png',
-      },
-    ],
-    summary: 'Apparently, it appears only to those people who are pure of heart and have a strong desire to see it.',
-  },
-  {
-    id: 78,
-    name: 'Rapidash',
-    type: 'Fire',
-    averageWeight: {
-      value: '95.0',
-      measurementUnit: 'kg',
-    },
-    image: 'https://cdn.bulbagarden.net/upload/5/58/Spr_5b_078.png',
-    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Rapidash_(Pok%C3%A9mon)',
-    foundAt: [
-      {
-        location: 'Kanto Route 28',
-        map: 'https://cdn.bulbagarden.net/upload/5/5b/Kanto_Route_28_Map.png',
-      },
-      {
-        location: 'Johto Mount Silver',
-        map: 'https://cdn.bulbagarden.net/upload/9/95/Johto_Mt_Silver_Map.png',
-      },
-    ],
-    summary: 'At full gallop, its four hooves barely touch the ground because it moves so incredibly fast.',
-  },
-  {
     id: 143,
     name: 'Snorlax',
     type: 'Normal',
@@ -219,8 +179,6 @@ const mockIsPokemonFavoriteById = {
   10: false,
   23: false,
   65: false,
-  151: false,
-  78: false,
   143: false,
   148: false,
 };
