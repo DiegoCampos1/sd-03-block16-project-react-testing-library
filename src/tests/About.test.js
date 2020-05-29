cd610154be748c68a100258ff70bbe098866fe28
renan.andreolle <renandreolle@hotmail.com>
2020-05-29 12:08:10 -0300

    About feito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import MemoryHistory from './MemoryHistory';
+import About from '../components/About';
+
+describe('Testando o About', () => {
+  it('Testando o texto e imagem', () => {
+    const { getByText, getByAltText } = MemoryHistory(<About />);
+    const sobre = getByText('About Pokédex');
+    expect(sobre).toBeInTheDocument();
+    const imagem = getByAltText('Pokédex');
+    expect(imagem.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -3,7 +3,7 @@ import { fireEvent } from '@testing-library/react';
 
 import MemoryHistory from './MemoryHistory';
 import Pokedex from '../components/Pokedex';
-
+// import data from '../data';
 
 describe('Testando a pokedex', () => {
   it('Testando o botão de próximo', () => {
@@ -12,10 +12,12 @@ describe('Testando a pokedex', () => {
     const type = getAllByTestId('pokemon-type-button');
     const encontrado = getByText('Encountered pokémons');
     const all = getByText('All');
+    const bug = getByText('Bug');
     fireEvent.click(prox);
     expect(prox).toBeInTheDocument();
     expect(type[0]).toBeInTheDocument();
     expect(encontrado).toBeInTheDocument();
     expect(all).toBeInTheDocument();
+    expect(bug).toBeInTheDocument();
   });
 });
