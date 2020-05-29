369c0431c96998176fd2da502d2f07a0fae4b5db
renan.andreolle <renandreolle@hotmail.com>
2020-05-29 10:54:28 -0300

    Fav foi

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import MemoryHistory from './MemoryHistory';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testando FavoritePokemons', () => {
+  it('Testando texto com nenhum favorito', () => {
+    const { getByText } = MemoryHistory(<FavoritePokemons />);
+    const texto = getByText('No favorite pokemon found');
+    expect(texto).toBeInTheDocument();
+  });
+});
