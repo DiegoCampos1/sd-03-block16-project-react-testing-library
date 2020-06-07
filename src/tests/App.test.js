8d23cc1e8aca230394b7aa14d4fae4de08964fee
Gabriel-O-Ferreira <gabriel1335555@gmail.com>
2020-06-07 00:16:52 -0300

    App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -11,4 +11,36 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test('Have Link Home in navBar', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const homeLink = getByText('Home');
+  fireEvent.click(homeLink);
+  expect(getByText(/Encountered pokémons/i));
+});
+test('should have About Link on home page and it redirects to About Page ', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const aboutLink = getByText(/About/);
+  fireEvent.click(aboutLink);
+  expect(getByText(/About Pokédex/i));
+});
+
+test('should have Favorite Pokemons Link on home page and it redirects to Favorites Page ', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const favoritesLink = getByText(/Favorite Pokémons/);
+  fireEvent.click(favoritesLink);
+  expect(getByText(/Favorite pokémons/));
 });
