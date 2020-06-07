7473a15f0f33b2985aa06c11338b103b0da80365
Gabriel-O-Ferreira <gabriel1335555@gmail.com>
2020-06-07 00:13:34 -0300

    NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+describe('when no refered adress should render an h2 and an image', () => {
+  test('message', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={[{ pathname: '/xablau' }]}>
+        <App />
+      </MemoryRouter>,
+    );
+    const notFoundMessage = getByText('Page requested not found');
+    expect(notFoundMessage).toBeInTheDocument();
+  });
+  test('should have pikachu image', () => {
+    const { getByAltText } = render(
+      <MemoryRouter initialEntries={[{ pathname: '/xablau' }]}>
+        <App />
+      </MemoryRouter>,
+    );
+    const image = getByAltText(/Pikachu crying because the page requested was not found/i);
+    expect(image.src).toMatch('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
