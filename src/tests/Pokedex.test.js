b4264506f0f1db77470236526a55e5e0cc9a3adb
Luma Arruda <luma.oarruda@gmail.com>
2020-06-07 23:00:43 -0300

    require 5 - Pokedex component tests

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,119 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, cleanup } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import data from '../data';
+
+const pokemonTypes = [
+  ...new Set(data.reduce((types, { type }) => [...types, type], [])),
+];
+
+function forEachPokemon(getByText) {
+  data.forEach(({ name }) => {
+    const nextPokemon = getByText('Próximo pokémon');
+    expect(getByText(name)).toBeInTheDocument();
+    fireEvent.click(nextPokemon);
+  });
+}
+
+afterEach(cleanup);
+
+describe('tests Pokedex.js', () => {
+  test('Verify the button next pokemon in page', () => {
+    const { getByText } = renderWithRouter(<App />, { route: '/'});
+
+    expect(getByText('Próximo pokémon')).toBeInTheDocument();
+  });
+
+  test('Verify if the next pokemon button shows next pokemon', () => {
+    const { getByText } = renderWithRouter(<App />, { route: '/'});
+    
+    forEachPokemon(getByText);
+  });
+
+  test('Verify if the when next button clicked in the last pokemon, returns to the start', () => {
+    const { getByText } = renderWithRouter(<App />, { route: '/'});
+    const nextButton = getByText('Próximo pokémon');
+
+    data.forEach(() => fireEvent.click(nextButton));
+
+    expect(getByText(data[0].name)).toBeInTheDocument();
+  });
+
+  test('Verify if the Pokedex shows just one pokemon at once', () => {
+    const { getAllByText } = renderWithRouter(<App />, { route: '/'});
+    const detailsButton = getAllByText('More details');
+
+    expect(detailsButton.length).toBe(1);
+  });
+
+  test('Verify filter buttons with the name equal the type', () => {
+    const { getAllByTestId } = renderWithRouter(<App />, { route: '/'});
+
+    pokemonTypes.forEach((type, index) => {
+      const typeButton = getAllByTestId('pokemon-type-button')[index];
+      expect(typeButton).toBeInTheDocument;
+      expect(typeButton).toHaveTextContent(type);
+    })
+  });
+
+  test('clink in a filter button should shows just pokemons of that type', () => {
+    const { getByText, getAllByTestId } = renderWithRouter(<App />, { route: '/'});
+
+    const nextButton = getByText('Próximo pokémon');
+
+    pokemonTypes.forEach((type, index) => {
+      const button = getAllByTestId('pokemon-type-button')[index];
+
+      fireEvent.click(button);
+
+      const filtredPokemons = data.filter((pokemon) => pokemon.type === type);
+
+      filtredPokemons.forEach((pokemon, _, array) => {
+        expect(getByText(pokemon.name)).toBeInTheDocument();
+        if (array.length > 1) fireEvent.click(nextButton);
+      });
+    });
+  });
+
+  test('Verify if Pokedex page has a reset button', () => {
+    const { getByText } = renderWithRouter(<App />, { route: '/'});
+
+    expect(getByText('All')).toBeInTheDocument();
+  });
+
+  test('Verify if All button shows all the pokemons', () => {
+    const { getByText } = renderWithRouter(<App />, { route: '/'});
+    const allButton = getByText('All');
+
+    fireEvent.click(allButton);
+
+    forEachPokemon(getByText);
+  });
+
+  test('Verify if the All button is loaded first', () => {
+    const { getByText } = renderWithRouter(<App />, { route: '/'});
+    
+    forEachPokemon(getByText);
+    expect(getByText(data[0].name)).toBeInTheDocument();
+  });
+
+  test('Verify all the types button', () => {
+    const { getAllByText, getByText } = renderWithRouter(<App />, { route: '/'});
+
+    pokemonTypes.forEach((type) => {
+      const typeButton = getAllByText(type)[1] || getByText(type);
+      expect(typeButton).toBeInTheDocument();
+    });
+  });
+
+  test('next pokemon button should disabled if theres only one pokemon', () => {
+    const { getByText } = renderWithRouter(<App />, { route: '/'});
+    const nextButton = getByText('Próximo pokémon');
+    const dragonTypeButton = getByText('Dragon');
+
+    fireEvent.click(dragonTypeButton);
+
+    expect(nextButton).toBeDisabled();
+  });
+});
