5bb64766b94fd06c64a7cf023ba05057822ae7ba
Mateus Moreira <ashuraw@hotmail.com>
2020-06-22 17:31:57 -0300

    getting 100%

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react'; // render in a container x
+import About from '../components/About';
+
+describe('file About.js', () => {
+  test('h2 heading with "About Pokédex" text', () => {
+    const { getByText } = render(<About />);
+    const heading = getByText('About Pokédex');
+    expect(heading).toBeInTheDocument();
+    expect(heading.tagName).toBe('H2');
+  });
+
+  test('2 paragraphs about Pokédex', () => {
+    render(<About />);
+    const paragraphs = document.querySelectorAll('p');
+    expect(paragraphs.length).toBe(2);
+  });
+
+  test('Pokédex image', () => {
+    const { getByAltText } = render(<About />);
+    const img = getByAltText('Pokédex');
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe(
+      'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+    );
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './MemoryHistory';
+import NotFound from '../components/NotFound';
+
+describe('Test NotFound.js', () => {
+  test('Finding h2 heading', () => {
+    const { getByText } = renderWithRouter(<NotFound />, { route: '/not-found' });
+    const notFound = getByText('Page requested not found');
+    expect(notFound).toBeInTheDocument();
+    expect(notFound.tagName).toBe('H2');
+  });
+
+  test('NotFound image', () => {
+    const { container } = renderWithRouter(<NotFound />, { route: '/not-found' });
+    const img = container.querySelector('IMG');
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
